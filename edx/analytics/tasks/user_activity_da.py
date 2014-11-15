538a21ef584dfefe71afcca07d4bd02a75875d6a
Jason Bau <jbau@stanford.edu>
2014-11-15 15:13:53 -0800

    add course filter non-direct-access event histograms

--- edx/analytics/tasks/user_activity_da.py
+++ edx/analytics/tasks/user_activity_da.py
@@ -1,15 +1,12 @@
 """Group events by institution and export them for research purposes"""
 
-import datetime
 import logging
 import json
 import luigi
 
-from collections import defaultdict
-from edx.analytics.tasks.mapreduce import MapReduceJobTask, MapReduceJobTaskMixin
-from edx.analytics.tasks.mysql_load import MysqlInsertTask
-from edx.analytics.tasks.pathutil import EventLogSelectionMixin, EventLogSelectionDownstreamMixin
-from edx.analytics.tasks.url import url_path_join, get_target_from_url, ExternalURL
+from edx.analytics.tasks.mapreduce import MapReduceJobTask
+from edx.analytics.tasks.pathutil import EventLogSelectionMixin
+from edx.analytics.tasks.url import url_path_join, get_target_from_url
 import edx.analytics.tasks.util.eventlog as eventlog
 import edx.analytics.tasks.util.opaque_key_util as opaque_key_util
 
@@ -44,63 +41,6 @@ class DirectAccessEventCountTask(EventLogSelectionMixin, MapReduceJobTask):
     name = luigi.Parameter()
     dest = luigi.Parameter()
 
-    def get_course_id(self, event):
-        """Gets course_id from event's data."""
-        # TODO: move into eventlog
-
-        # Get the event data:
-        event_context = event.get('context')
-        if event_context is None:
-            # Assume it's old, and not worth logging...
-            return None
-
-        # Get the course_id from the data, and validate.
-        course_id = event_context.get('course_id', '')
-        if not course_id:
-            return None
-
-        if not opaque_key_util.is_valid_course_id(course_id):
-            log.error("encountered event with bogus course_id: %s", event)
-            return None
-
-        return course_id
-
-    def get_predicate_labels(self, event):
-        """Override this with calculation of one or more possible labels for the current event."""
-        raise NotImplementedError
-
-    def get_mapper_key(self, _course_id, _username, _date_string):
-        """Return a tuple representing the key to use for a log entry, or None if skipping."""
-        raise NotImplementedError
-
-    def get_mapper_value(self, _course_id, _username, _date_string, label):
-        """Return a tuple representing the value to use for a log entry."""
-        raise NotImplementedError
-
-    def _encode_tuple(self, values):
-        """
-        Convert values into a tuple containing encoded strings.
-
-        Parameters:
-            Values is a list or tuple.
-
-        This enforces a standard encoding for the parts of the
-        key. Without this a part of the key might appear differently
-        in the key string when it is coerced to a string by luigi. For
-        example, if the same key value appears in two different
-        records, one as a str() type and the other a unicode() then
-        without this change they would appear as u'Foo' and 'Foo' in
-        the final key string. Although python doesn't care about this
-        difference, hadoop does, and will bucket the values
-        separately. Which is not what we want.
-        """
-        # TODO: refactor this into a utility function and update jobs
-        # to always UTF8 encode mapper keys.
-        if len(values) > 1:
-            return tuple([value.encode('utf8') for value in values])
-        else:
-            return values[0].encode('utf8')
-
     def mapper(self, line):
         """Default mapper implementation, that always outputs the log line, but with a configurable key."""
         try:
@@ -113,7 +53,7 @@ class DirectAccessEventCountTask(EventLogSelectionMixin, MapReduceJobTask):
         event, date_string = value
 
         username = event.get('username', '').strip()
-        if not username or not username.startswith("anon__"):
+        if not username or not username.startswith("anon__"):  # filter out non-direct-access user
             return
 
         evt_time = event.get('time')
@@ -263,6 +203,157 @@ class DirectAccessLifetimeHist(EventLogSelectionMixin, MapReduceJobTask):
         output_name = u'direct_access_session_lifetime_hist_{name}/'.format(name=self.name)
         log.info(output_name)
         return get_target_from_url(url_path_join(self.dest, output_name))
+
+
+class CourseFilterEventCountTask(EventLogSelectionMixin, MapReduceJobTask):
+    """
+    Base class to extract events from logs over a selected interval of time.
+
+    This class provides a parameterized mapper.  Derived classes must
+    override the predicates applied to events, and the definition of
+    what is a key and what a value from the mapper.  Derived classes
+    also define the reducer that consumes the mapper output.
+
+    Parameters:
+        output_root: Directory to store the output in.
+
+        The following are defined in EventLogSelectionMixin:
+
+        source: A URL to a path that contains log files that contain the events.
+        interval: The range of dates to export logs for.
+        pattern: A regex with a named capture group for the date that approximates the date that the events within were
+            emitted. Note that the search interval is expanded, so events don't have to be in exactly the right file
+            in order for them to be processed.
+    """
+    name = luigi.Parameter()
+    dest = luigi.Parameter()
+    course_ids_allowed = {
+        "DB/Constraints/SelfPaced",
+        "DB/Indexes/SelfPaced",
+        "DB/JSON/SelfPaced",
+        "DB/OLAP/SelfPaced",
+        "DB/RA/SelfPaced",
+        "DB/RD/SelfPaced",
+        "DB/RDB/SelfPaced",
+        "DB/Recursion/SelfPaced",
+        "DB/SQL/SelfPaced",
+        "DB/UML/SelfPaced",
+        "DB/Views/SelfPaced",
+        "DB/XML/SelfPaced",
+        "DB/XPath/SelfPaced",
+        "DB/XSLT/SelfPaced",
+        "Education/OpenKnowledge/Fall2014",
+        "Engineering/CS144/Introduction_to_Computer_Networking",
+        "Engineering/Networking/Winter2014",
+        "JaneU/test2/JaneTest2",
+        "SampleUniversity/102/Fall2013",
+        "StanfordOnline/JS101/2014",
+        "StanfordOnline/OpenEdX/Demo",
+    }
+
+    def get_course_id(self, event):
+        """Gets course_id from event's data."""
+        # TODO: move into eventlog
+
+        # Get the event data:
+        event_context = event.get('context')
+        if event_context is None:
+            # Assume it's old, and not worth logging...
+            return None
+
+        # Get the course_id from the data, and validate.
+        course_id = event_context.get('course_id', '')
+        if not course_id:
+            return None
+
+        if not opaque_key_util.is_valid_course_id(course_id):
+            log.error("encountered event with bogus course_id: %s", event)
+            return None
+
+        return course_id
+
+    def mapper(self, line):
+        """Default mapper implementation, that always outputs the log line, but with a configurable key."""
+        try:
+            value = self.get_event_and_date_string(line)
+        except TypeError:
+            return
+
+        if value is None:
+            return
+        event, date_string = value
+
+        username = event.get('username', '').strip()
+        if not username or username.startswith("anon__"):  # filter out direct access
+            return
+
+        if self.get_course_id(event) not in self.course_ids_allowed:
+            return
+
+        evt_time = event.get('time')
+        if not evt_time:
+            return
+
+        labels = extract_predicate_labels(event)
+
+        for label in labels:
+            if label == PLAY_VIDEO_LABEL:
+                evt_evt = json.loads(event.get('event', "{}"))
+#                log.info(evt_evt.get('id'))
+                yield username, (label, evt_evt.get('id'))
+            elif label == PROBLEM_LABEL:
+#                log.info(event.get('event', "{}"))
+                evt_evt = event.get('event', {})
+                yield username, (label, evt_evt.get('problem_id'))
+            elif label == POST_FORUM_LABEL:
+                yield username, (label, "posted")
+
+    def reducer(self, key, values):
+        values_set = set(values)
+
+        data = {
+            PLAY_VIDEO_LABEL: 0,
+            PROBLEM_LABEL: 0,
+            POST_FORUM_LABEL: 0,
+        }
+        for value in values_set:
+            data[value[0]] += 1
+
+        for label in data:
+            yield key, label, data[label]
+
+    def output(self):
+        output_name = u'course_filter_event_count_{name}/'.format(name=self.name)
+        log.info(output_name)
+        return get_target_from_url(url_path_join(self.dest, output_name))
+
+
+class CourseFilterEventCountHist(EventLogSelectionMixin, MapReduceJobTask):
+    name = luigi.Parameter()
+    dest = luigi.Parameter()
+
+    def requires(self):
+        results = {
+            'users': CourseFilterEventCountTask(
+                mapreduce_engine=self.mapreduce_engine,
+                interval=self.interval,
+                name=self.name,
+                dest=self.dest,
+            ),
+        }
+        return results
+
+    def output(self):
+        output_name = u'course_filter_event_histogram_{name}/'.format(name=self.name)
+        log.info(output_name)
+        return get_target_from_url(url_path_join(self.dest, output_name))
+
+    def mapper(self, line):
+        username, label, count = line.split('\t')
+        yield (label, count), username
+
+    def reducer(self, key, values):
+        yield key, len(list(values))
 
 
 def extract_predicate_labels(event):
