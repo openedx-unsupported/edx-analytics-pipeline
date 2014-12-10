5f62c9e5a5bc4419c0ab3c2de5bb7f1c9b798864
Gabe Mulley <gabe@edx.org>
2014-12-10 15:17:46 -0500

    User activity aggregates

--- edx/analytics/tasks/tests/test_user_activity.py
+++ edx/analytics/tasks/tests/test_user_activity.py
@@ -2,32 +2,35 @@
 Tests for tasks that collect enrollment events.
 
 """
+import datetime
 import json
 
 from luigi import date_interval
 
 from edx.analytics.tasks.user_activity import (
-    UserActivityPerIntervalTask,
+    UserActivityTask,
+    CourseActivityWeeklyTask,
+    CourseActivityMonthlyTask,
     ACTIVE_LABEL,
     PROBLEM_LABEL,
     PLAY_VIDEO_LABEL,
     POST_FORUM_LABEL,
-    CountLastElementMixin,
 )
 
 from edx.analytics.tasks.tests import unittest
 from edx.analytics.tasks.tests.opaque_key_mixins import InitializeOpaqueKeysMixin, InitializeLegacyKeysMixin
 
 
-class UserActivityPerIntervalMapTest(InitializeOpaqueKeysMixin, unittest.TestCase):
+class UserActivityTaskMapTest(InitializeOpaqueKeysMixin, unittest.TestCase):
     """
     Tests to verify that event log parsing by mapper works correctly.
     """
     def setUp(self):
         self.initialize_ids()
         self.interval_string = '2013-12-01-2013-12-31'
-        self.task = UserActivityPerIntervalTask(
+        self.task = UserActivityTask(
             interval=date_interval.Custom.parse(self.interval_string),
+            output_root='/tmp/foo'
         )
         # We're not really using Luigi properly here to create
         # the task, so set up manually.
@@ -35,7 +38,7 @@ class UserActivityPerIntervalMapTest(InitializeOpaqueKeysMixin, unittest.TestCas
 
         self.username = "test_user"
         self.timestamp = "2013-12-17T15:38:32.805444"
-        self.expected_interval_string = '2013-12-17-2013-12-24'
+        self.expected_date_string = '2013-12-17'
         self.event_type = "edx.dummy.event"
 
     def _create_event_log_line(self, **kwargs):
@@ -105,28 +108,28 @@ class UserActivityPerIntervalMapTest(InitializeOpaqueKeysMixin, unittest.TestCas
     def test_good_dummy_event(self):
         line = self._create_event_log_line()
         event = tuple(self.task.mapper(line))
-        expected = (((self.course_id, self.username, self.expected_interval_string), ACTIVE_LABEL),)
+        expected = (((self.course_id, self.username, self.expected_date_string, ACTIVE_LABEL), 1),)
         self.assertEquals(event, expected)
 
     def test_play_video_event(self):
         line = self._create_event_log_line(event_source='browser', event_type='play_video')
         event = tuple(self.task.mapper(line))
-        expected = (((self.course_id, self.username, self.expected_interval_string), ACTIVE_LABEL),
-                    ((self.course_id, self.username, self.expected_interval_string), PLAY_VIDEO_LABEL))
+        expected = (((self.course_id, self.username, self.expected_date_string, ACTIVE_LABEL), 1),
+                    ((self.course_id, self.username, self.expected_date_string, PLAY_VIDEO_LABEL), 1))
         self.assertEquals(event, expected)
 
     def test_problem_event(self):
         line = self._create_event_log_line(event_source='server', event_type='problem_check')
         event = tuple(self.task.mapper(line))
-        expected = (((self.course_id, self.username, self.expected_interval_string), ACTIVE_LABEL),
-                    ((self.course_id, self.username, self.expected_interval_string), PROBLEM_LABEL))
+        expected = (((self.course_id, self.username, self.expected_date_string, ACTIVE_LABEL), 1),
+                    ((self.course_id, self.username, self.expected_date_string, PROBLEM_LABEL), 1))
         self.assertEquals(event, expected)
 
     def test_post_forum_event(self):
         line = self._create_event_log_line(event_source='server', event_type='blah/blah/threads/create')
         event = tuple(self.task.mapper(line))
-        expected = (((self.course_id, self.username, self.expected_interval_string), ACTIVE_LABEL),
-                    ((self.course_id, self.username, self.expected_interval_string), POST_FORUM_LABEL))
+        expected = (((self.course_id, self.username, self.expected_date_string, ACTIVE_LABEL), 1),
+                    ((self.course_id, self.username, self.expected_date_string, POST_FORUM_LABEL), 1))
         self.assertEquals(event, expected)
 
     def test_exclusion_of_events_by_source(self):
@@ -147,9 +150,10 @@ class UserActivityPerIntervalMapTest(InitializeOpaqueKeysMixin, unittest.TestCas
             line = self._create_event_log_line(event_source='server', event_type=event_type)
             self.assert_no_output_for(line)
 
-    def test_multiple_weeks(self):
+    def test_multiple(self):
         lines = [
             self._create_event_log_line(event_source='browser', event_type='play_video'),
+            self._create_event_log_line(event_source='mobile', event_type='play_video'),
             self._create_event_log_line(
                 time="2013-12-24T00:00:00.000000", event_source='server', event_type='problem_check'),
             self._create_event_log_line(time="2013-12-16T04:00:00.000000")
@@ -160,16 +164,18 @@ class UserActivityPerIntervalMapTest(InitializeOpaqueKeysMixin, unittest.TestCas
                 outputs.append(output)
 
         expected = (
-            ((self.course_id, self.username, self.expected_interval_string), ACTIVE_LABEL),
-            ((self.course_id, self.username, self.expected_interval_string), PLAY_VIDEO_LABEL),
-            ((self.course_id, self.username, '2013-12-24-2013-12-31'), ACTIVE_LABEL),
-            ((self.course_id, self.username, '2013-12-24-2013-12-31'), PROBLEM_LABEL),
-            ((self.course_id, self.username, '2013-12-10-2013-12-17'), ACTIVE_LABEL),
+            ((self.course_id, self.username, self.expected_date_string, ACTIVE_LABEL), 1),
+            ((self.course_id, self.username, self.expected_date_string, PLAY_VIDEO_LABEL), 1),
+            ((self.course_id, self.username, self.expected_date_string, ACTIVE_LABEL), 1),
+            ((self.course_id, self.username, self.expected_date_string, PLAY_VIDEO_LABEL), 1),
+            ((self.course_id, self.username, '2013-12-24', ACTIVE_LABEL), 1),
+            ((self.course_id, self.username, '2013-12-24', PROBLEM_LABEL), 1),
+            ((self.course_id, self.username, '2013-12-16', ACTIVE_LABEL), 1),
         )
         self.assertItemsEqual(outputs, expected)
 
 
-class UserActivityPerIntervalLegacyMapTest(InitializeLegacyKeysMixin, UserActivityPerIntervalMapTest):
+class UserActivityTaskMapLegacyTest(InitializeLegacyKeysMixin, UserActivityTaskMapTest):
     """Tests to verify that event log parsing by mapper works correctly with legacy ids."""
     pass
 
@@ -182,65 +188,103 @@ class UserActivityPerIntervalReduceTest(InitializeOpaqueKeysMixin, unittest.Test
         self.initialize_ids()
         self.username = 'test_user'
         self.interval_string = '2013-12-01-2013-12-31'
-        self.task = UserActivityPerIntervalTask(
+        self.task = UserActivityTask(
             interval=date_interval.Custom.parse(self.interval_string),
+            output_root='/tmp/foo'
         )
-        self.key = (self.course_id, self.username, self.interval_string)
+        self.key = (self.course_id, self.username, '2013-12-04')
 
-    def _get_reducer_output(self, values):
+    def _get_reducer_output(self, key, values):
         """Run reducer with provided values hardcoded key."""
-        return tuple(self.task.reducer(self.key, values))
+        return tuple(self.task.reducer(key, values))
 
-    def _check_output(self, inputs, expected):
+    def _check_output(self, key, values, expected):
         """Compare generated with expected output."""
-        self.assertEquals(self._get_reducer_output(inputs), expected)
+        self.assertEquals(self._get_reducer_output(key, values), expected)
 
     def test_no_events(self):
-        self._check_output([], tuple())
+        self._check_output(tuple(), [], tuple())
 
     def test_single_event(self):
-        inputs = [ACTIVE_LABEL]
-        expected = ((self.course_id, '2013-12-01', '2013-12-31', ACTIVE_LABEL, self.username),)
-        self._check_output(inputs, expected)
+        key = (self.course_id, self.username, '2013-12-01', ACTIVE_LABEL)
+        values = [1]
+        expected = (((self.course_id, self.username, '2013-12-01', ACTIVE_LABEL), 1),)
+        self._check_output(key, values, expected)
 
-    def test_multiple_events_different(self):
-        inputs = [ACTIVE_LABEL, PROBLEM_LABEL]
-        expected = ((self.course_id, '2013-12-01', '2013-12-31', ACTIVE_LABEL, self.username),
-                    (self.course_id, '2013-12-01', '2013-12-31', PROBLEM_LABEL, self.username),)
-        self._check_output(inputs, expected)
-
-    def test_multiple_events_with_dupes(self):
-        inputs = [ACTIVE_LABEL, PROBLEM_LABEL, ACTIVE_LABEL, PROBLEM_LABEL]
-        expected = ((self.course_id, '2013-12-01', '2013-12-31', ACTIVE_LABEL, self.username),
-                    (self.course_id, '2013-12-01', '2013-12-31', PROBLEM_LABEL, self.username),)
-        self._check_output(inputs, expected)
+    def test_multiple(self):
+        key = (self.course_id, self.username, '2013-12-01', ACTIVE_LABEL)
+        values = [1, 2, 1]
+        expected = (((self.course_id, self.username, '2013-12-01', ACTIVE_LABEL), 4),)
+        self._check_output(key, values, expected)
 
 
-class CountLastElementMixinTest(unittest.TestCase):
-    """
-    Tests to verify that CountLastElementMixin works correctly.
-    """
+class CourseActivityWeeklyTaskTest(InitializeOpaqueKeysMixin, unittest.TestCase):
+    """Ensure the date interval is computed correctly for monthly tasks."""
+
     def setUp(self):
-        self.interval_string = '2013-12-01-2013-12-31'
-        self.task = CountLastElementMixin()
-        self.key = ('course', '2013-01-01')
+        self.initialize_ids()
 
-    def _get_reducer_output(self, values):
-        """Run reducer with provided values hardcoded key."""
-        return tuple(self.task.reducer(self.key, values))
+    def test_zero_weeks(self):
+        task = CourseActivityWeeklyTask(
+            end_date=datetime.date(2014, 1, 1),
+            weeks=0
+        )
+        with self.assertRaises(ValueError):
+            task.interval
 
-    def test_mapper(self):
-        inputs = ['a', 'b', 'c', 'd']
-        expected = ((('a', 'b', 'c'), 1),)
-        output = tuple(self.task.mapper('\t'.join(inputs)))
-        self.assertEquals(output, expected)
+    def test_single_week(self):
+        task = CourseActivityWeeklyTask(
+            end_date=datetime.date(2014, 1, 1),
+            weeks=1
+        )
+        self.assertEquals(task.interval, date_interval.Custom.parse('2013-12-23-2013-12-30'))
 
-    def test_no_counts(self):
-        self.assertEquals(self._get_reducer_output([]), ((self.key, 0),))
+    def test_multi_week(self):
+        task = CourseActivityWeeklyTask(
+            end_date=datetime.date(2014, 1, 6),
+            weeks=2
+        )
+        self.assertEquals(task.interval, date_interval.Custom.parse('2013-12-23-2014-01-06'))
 
-    def test_single_count(self):
-        self.assertEquals(self._get_reducer_output([1]), ((self.key, 1),))
+    def test_leap_year(self):
+        task = CourseActivityWeeklyTask(
+            end_date=datetime.date(2012, 2, 29),
+            weeks=52
+        )
+        self.assertEquals(task.interval, date_interval.Custom.parse('2011-02-28-2012-02-27'))
 
-    def test_multiple_counts(self):
-        inputs = [1, 1, 1, 1, 1]
-        self.assertEquals(self._get_reducer_output(inputs), ((self.key, 5),))
+
+class CourseActivityMonthlyTaskTest(InitializeOpaqueKeysMixin, unittest.TestCase):
+    """Ensure the date interval is computed correctly for monthly tasks."""
+
+    def setUp(self):
+        self.initialize_ids()
+
+    def test_zero_months(self):
+        task = CourseActivityMonthlyTask(
+            end_date=datetime.date(2014, 1, 31),
+            months=0
+        )
+        with self.assertRaises(ValueError):
+            task.interval
+
+    def test_single_month(self):
+        task = CourseActivityMonthlyTask(
+            end_date=datetime.date(2014, 1, 31),
+            months=1
+        )
+        self.assertEquals(task.interval, date_interval.Custom.parse('2013-12-01-2014-01-01'))
+
+    def test_multi_month(self):
+        task = CourseActivityMonthlyTask(
+            end_date=datetime.date(2014, 1, 31),
+            months=2
+        )
+        self.assertEquals(task.interval, date_interval.Custom.parse('2013-11-01-2014-01-01'))
+
+    def test_leap_year(self):
+        task = CourseActivityMonthlyTask(
+            end_date=datetime.date(2012, 2, 29),
+            months=12
+        )
+        self.assertEquals(task.interval, date_interval.Custom.parse('2011-02-01-2012-02-01'))
