5438945eb17807b82fc37f3c6931a011cc036d46
John Baker <john@edx.org>
2015-04-16 11:23:10 -0400

    Initial version of DailyStudentEngagementAcceptanceTest class.

--- edx/analytics/tasks/tests/acceptance/test_student_engagement.py
+++ edx/analytics/tasks/tests/acceptance/test_student_engagement.py
@@ -1,8 +1,9 @@
 """
-End to end test of answer distribution.
+End to end test of student engagement.
 """
 
 import os
+import datetime
 import logging
 
 from luigi.s3 import S3Target
@@ -10,116 +11,133 @@ from luigi.s3 import S3Target
 from edx.analytics.tasks.tests.acceptance import AcceptanceTestCase
 from edx.analytics.tasks.url import url_path_join
 
+import pandas as pd
+
 
 log = logging.getLogger(__name__)
 
 
-class BaseStudentEngagementAcceptanceTest(AcceptanceTestCase):
-    """Base class for setting up answer dist acceptance tests"""
+class DailyStudentEngagementAcceptanceTest(AcceptanceTestCase):
+    # Acceptance test for the CSV-generating Student Engagement Task
 
     INPUT_FILE = 'student_engagement_acceptance_tracking.log'
     INPUT_FORMAT = 'oddjob.ManifestTextInputFormat'
     NUM_REDUCERS = 1
 
-    def setUp(self):
-        super(BaseStudentEngagementAcceptanceTest, self).setUp()
-
-        assert 'oddjob_jar' in self.config
-
-        self.oddjob_jar = self.config['oddjob_jar']
-
-        self.upload_data()
-
-    def upload_data(self):
-       src = os.path.join(self.data_dir, 'input', self.INPUT_FILE)
-       dst = url_path_join(self.test_src, self.INPUT_FILE)
-
-       self.fail(src)
-       #print "dest: " + dst
-
-       #  Upload test data file
-       self.s3_client.put(src, dst)
-
-    # def test_foo(self):
-    #     src = os.path.join(self.data_dir, 'input', self.INPUT_FILE)
-    #     dst = url_path_join(self.test_src, self.INPUT_FILE)
-    #
-    #     # jab: dst = s3://edx-analytics-test-output/automation/93c61ab6a10264656e6bcce0fc4265d2/StudentEngagementAcceptanceTest/src/student_engagement_acceptance_tracking.log
-    #     # jab: src = /Users/johnbaker/code/edx-analytics-pipeline/edx/analytics/tasks/tests/acceptance/fixtures/input/student_engagement_acceptance_tracking.log
-    #     self.fail(src)
-
-
-class StudentEngagementAcceptanceTest(BaseStudentEngagementAcceptanceTest):
-    """Acceptance test for the CSV-generating Student Engagement Task"""
-
     def test_student_engagement(self):
-        self.task.launch([
-            'StudentEngagementOneFilePerStudentTask',
-            '--src', self.test_src,
-            '--dest', url_path_join(self.test_root, 'dst'),
-            '--name', 'test',
-            '--output-root', self.test_out,
-            '--include', '"*"',
-            '--manifest', url_path_join(self.test_root, 'manifest.txt'),
-            '--base-input-format', self.INPUT_FORMAT,
-            '--lib-jar', self.oddjob_jar,
-            '--n-reduce-tasks', str(self.NUM_REDUCERS),
-        ])
-        self.validate_output()
+        self.upload_tracking_log(self.INPUT_FILE, datetime.date(2015, 4, 05))
+        self.execute_sql_fixture_file('load_student_engagement.sql')
 
-    def validate_output(self):
+        self.task.launch([
+            'StudentEngagementCsvFileTask',
+            '--source', self.test_src,
+            '--output-root', self.test_out,
+            '--n-reduce-tasks', str(self.NUM_REDUCERS),
+            '--interval', '2015-04-05-2015-04-20',
+        ])
+
+        #
+        # Produce student_engagement file list
+        # Eg:
+        # s3://.../StudentEngagementAcceptanceTest/out/.../student_engagement_daily_2015-04-05.csv
+        #
         outputs = self.s3_client.list(self.test_out)
         outputs = [url_path_join(self.test_out, p) for p in outputs]
 
-        #self.fail(outputs)
-        #self.fail("foobarbaz")
-
-#
-#         # There are 3 courses in the test data
-        self.assertEqual(len(outputs), 3)
+        # There are 15 student_engagement files in the test data directory
+        self.assertEqual(len(outputs), 15)
 
         # Check that the results have data
-        for output in outputs:
-            with S3Target(output).open() as f:
-                lines = [l for l in f][1:]  # Skip header
-                self.assertTrue(len(lines) > 0)
+        for output in outputs: # 'file' is a builtin type, 'name' is a less-ambiguous variable name.
+            try:
+                with S3Target(output).open() as f:
 
-                # Check that at least one of the count columns is non zero
-                get_count = lambda line: int(line.split(',')[3])
-                self.assertTrue(any(get_count(l) > 0 for l in lines))
+                    # Construct dataframe from file to create more intuitive column handling
+                    df = pd.read_csv(f)
+
+                columns = len(df.columns)
+                rows    = len(df)
 
 
-# class AnswerDistributionMysqlAcceptanceTests(BaseStudentEngagementAcceptanceTest):
-#     """Acceptance tests for Answer Distribution Tasks -> MySQL"""
-#
-#     def test_answer_distribution_mysql(self):
-#         self.task.launch([
-#             'AnswerDistributionToMySQLTaskWorkflow',
-#             '--src', self.test_src,
-#             '--dest', url_path_join(self.test_root, 'dst'),
-#             '--name', 'test',
-#             '--include', '"*"',
-#             '--manifest', url_path_join(self.test_root, 'manifest.txt'),
-#             '--base-input-format', self.INPUT_FORMAT,
-#             '--lib-jar', self.oddjob_jar,
-#             '--n-reduce-tasks', str(self.NUM_REDUCERS),
-#             '--credentials', self.export_db.credentials_file_url,
-#         ])
-#
-#         self.validate_output()
-#
-#     def validate_output(self):
-#         with self.export_db.cursor() as cursor:
-#             cursor.execute('SELECT DISTINCT(`course_id`) from answer_distribution')
-#             uniq_course_ids = cursor.fetchall()
-#             # There are 3 courses in the test data
-#             self.assertEqual(len(uniq_course_ids), 3)
-#
-#             # the fetchall above returns a list of singleton tuples, so we use course_id[0] below
-#             for course_id in uniq_course_ids:
-#                 cursor.execute(
-#                     'SELECT COUNT(*) FROM answer_distribution where `course_id`="{}" and `count`>0'.format(course_id[0])
-#                 )
-#                 count = cursor.fetchone()[0]
-#                 # Check that at least one of the count columns is non zero
-#                 self.assertGreaterEqual(count, 1)
+                #
+                # Ensure each student engagement file has the correct number of columns (15)
+                #
+                self.validate_number_of_columns(columns)
+
+                #
+                # Ensure date on each row is of the format yyyy-mm-dd
+                #
+                for date in df["date"]:
+                    self.validate_date_cell_format(date)
+
+                #
+                # Ensure course_id on each row is of the format characters/characters/characters
+                #
+                for course_id in df["course_id"]:
+                    self.validate_course_id_string_format(course_id)
+
+                #
+                # Ensure user_name on each row is of the format characters/characters/characters
+                #
+                for user_name in df["username"]:
+                    self.validate_username_string_format(user_name)
+
+                #
+                # Ensure email address on each row is of the format characters/characters/characters
+                #
+                for email in df["email"]:
+                    self.validate_email_string_format(email)
+
+                #
+                # Ensure problems,videos,forum and texbook column values are greater than 0
+                #
+                for column_name in df.ix[:,5:14]:
+                    for r in df[column_name]:
+                        self.validate_problems_videos_forums_textbook_values(r)
+
+                #
+                # Cohort is present or not
+                #
+                for cohort in df["cohort"]:
+                    self.validate_cohort_format(cohort)
+
+
+                #
+                # Validate various comparisons within a given row. Eg:
+                # 1. problems correct gte to problems_attempted
+                # 2.
+                #
+                self.validate_within_rows(df)
+
+
+            except IOError as exc:
+                if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
+                    raise # Propagate other kinds of IOError.
+
+
+    def validate_number_of_columns(self,num_columns):
+        self.assertTrue(num_columns==15,msg="Number of columns not equal to 15")
+
+    def validate_date_cell_format(self,date):
+        self.assertRegexpMatches(date,'^\d\d\d\d-\d\d-\d\d$')
+
+    def validate_course_id_string_format(self,course_id):
+        self.assertRegexpMatches(course_id,'^.{1,}\/.{1,}\/.{1,}$')
+
+    def validate_username_string_format(self,user_name):
+        self.assertRegexpMatches(user_name,'^.{1,}$')
+
+    def validate_email_string_format(self,email):
+        self.assertRegexpMatches(email,'^([^@|\s]+@[^@]+\.[^@|\s]+)$')
+
+    def validate_cohort_format(self,cohort):
+        self.assertRegexpMatches(str(cohort),'^.*$')
+
+    def validate_problems_videos_forums_textbook_values(self,value):
+        self.assertTrue(int(value) >= 0, msg="Problems,Videos,Forums or Textbook fields are not greater or equal to 0.")
+
+    def validate_within_rows(self,dataframe):
+
+        for index, row in dataframe.iterrows():
+            # Number of correct problems should be equal to or lower than problems_attempted
+            self.assertTrue(row["problems_correct"] <= row["problems_attempted"],msg="Greater number of problems_correct than problems_attempted.")
