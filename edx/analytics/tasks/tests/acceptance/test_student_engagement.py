70020e4e599d93deb2393266317374c48f3a1d03
Brian Wilson <brian@edx.org>
2015-04-22 18:23:11 -0400

    Pull dates out of csv paths, and check for zero counts.

--- edx/analytics/tasks/tests/acceptance/test_student_engagement.py
+++ edx/analytics/tasks/tests/acceptance/test_student_engagement.py
@@ -5,6 +5,7 @@ End to end test of student engagement.
 import datetime
 import hashlib
 import logging
+import re
 
 from luigi.s3 import S3Target
 
@@ -29,6 +30,24 @@ class DailyStudentEngagementAcceptanceTest(AcceptanceTestCase):
     COURSE_3 = "course-v1:edX+DemoX+Demo_Course_2015"
 
     ALL_COURSES = [COURSE_1, COURSE_2, COURSE_3]
+
+    # We only expect some of the generated files to have any counts at all, so enumerate them.
+    NONZERO_OUTPUT = [
+        (COURSE_1, '2015-04-13', 'daily'),
+        (COURSE_1, '2015-04-16', 'daily'),
+        (COURSE_2, '2015-04-16', 'daily'),
+        (COURSE_3, '2015-04-09', 'daily'),
+        (COURSE_3, '2015-04-12', 'daily'),
+        (COURSE_3, '2015-04-13', 'daily'),
+        (COURSE_3, '2015-04-16', 'daily'),
+        (COURSE_1, '2015-04-19', 'weekly'),
+        (COURSE_2, '2015-04-19', 'weekly'),
+        (COURSE_3, '2015-04-12', 'weekly'),
+        (COURSE_3, '2015-04-19', 'weekly'),
+        (COURSE_1, '2015-04-19', 'all'),
+        (COURSE_2, '2015-04-19', 'all'),
+        (COURSE_3, '2015-04-19', 'all'),
+    ]
 
     def test_student_engagement(self):
         self.upload_tracking_log(self.INPUT_FILE, datetime.date(2015, 4, 10))
@@ -55,7 +74,7 @@ class DailyStudentEngagementAcceptanceTest(AcceptanceTestCase):
             '--interval-type', 'weekly',
         ])
 
-        self.test_all = url_path_join(self.test_out, 'daily')
+        self.test_all = url_path_join(self.test_out, 'all')
         self.task.launch([
             'StudentEngagementCsvFileTask',
             '--source', self.test_src,
@@ -91,40 +110,62 @@ class DailyStudentEngagementAcceptanceTest(AcceptanceTestCase):
                 # Check that the results have data
                 for output in outputs:
 
-                    # TODO: parse expected date from output.
+                    # parse expected date from output.
+                    if interval_type == 'all':
+                        expected_date = '2015-04-19'
+                    else:
+                        csv_pattern = '.*student_engagement_.*_(\\d\\d\\d\\d-\\d\\d-\\d\\d)\\.csv'
+                        match = re.match(csv_pattern, output)
+                        expected_date = match.group(1)
 
-
-                    df = []
-                    with S3Target(output).open() as f:
+                    dataframe = []
+                    with S3Target(output).open() as csvfile:
                         # Construct dataframe from file to create more intuitive column handling
-                        df = pd.read_csv(f)
+                        dataframe = pd.read_csv(csvfile)
+                        dataframe.fillna('', inplace=True)
 
-                    columns = len(df.columns)
-                    # rows = len(df)
+                    # General validation:
+                    self.validate_number_of_columns(len(dataframe.columns))
 
-                    self.validate_number_of_columns(columns)
-
-                    for date in df[date_column_name]:
+                    for date in dataframe[date_column_name]:
                         self.validate_date_cell_format(date)
-                        self.assertEquals(date, df[date_column_name][0])
 
-                    for row_course_id in df["course_id"]:
-                        self.assertEquals(row_course_id, course_id)
-
-                    for user_name in df["username"]:
+                    for user_name in dataframe["username"]:
                         self.validate_username_string_format(user_name)
 
-                    for email in df["email"]:
+                    for email in dataframe["email"]:
                         self.validate_email_string_format(email)
 
-                    for column_name in df.ix[:, 5:14]:
-                        for r in df[column_name]:
-                            self.validate_problems_videos_forums_textbook_values(r)
-
-                    for cohort in df["cohort"]:
+                    for cohort in dataframe["cohort"]:
                         self.validate_cohort_format(cohort)
 
-                    self.validate_within_rows(df)
+                    for column_name in dataframe.ix[:, 5:14]:
+                        for column_value in dataframe[column_name]:
+                            self.validate_problems_videos_forums_textbook_values(column_value)
+
+                    self.validate_within_rows(dataframe)
+
+                    # Validate specific values:
+                    for date in dataframe[date_column_name]:
+                        self.assertEquals(date, expected_date)
+
+                    for row_course_id in dataframe["course_id"]:
+                        self.assertEquals(row_course_id, course_id)
+
+                    if (course_id, expected_date, interval_type) in self.NONZERO_OUTPUT:
+                        # TODO: read expected values from fixture and compare:
+                        pass
+                    else:
+                        self.assert_zero_engagement(dataframe)
+                        # TODO: check username, email, and cohort names (if any).
+
+    def assert_zero_engagement(self, dataframe):
+        """Asserts that all counts are zero."""
+        for column_name in dataframe.columns[5:14]:
+            for column_value in dataframe[column_name]:
+                self.assertEquals(column_value, 0)
+        for column_value in dataframe['last_subsection_viewed']:
+            self.assertEquals(len(column_value), 0)
 
     def validate_number_of_columns(self,num_columns):
         """Ensure each student engagement file has the correct number of columns (15)"""
