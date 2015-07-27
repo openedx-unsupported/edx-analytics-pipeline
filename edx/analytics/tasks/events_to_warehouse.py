f2f0d1f29305adcb8f1e49cf7b879e55dab49ae8
jamesrowan <jrowan@edx.org>
2015-07-27 15:43:09 -0400

    began acceptance test work

--- edx/analytics/tasks/events_to_warehouse.py
+++ edx/analytics/tasks/events_to_warehouse.py
@@ -1,84 +1,13 @@
 """Task for loading cleaned, gzipped event log data into Vertica."""
 import datetime
-import os
 import luigi
 import logging
-import gzip
-# from edx.analytics.tasks.clean_for_vertica import CleanForVerticaTask
 from edx.analytics.tasks.url import get_target_from_url, url_path_join
 
 from vertica_load import VerticaCopyTask, VerticaCopyTaskMixin
 from clean_for_vertica import CleanForVerticaTask
-import vertica_python
 
 log = logging.getLogger(__name__)
-
-class DummyTarget(luigi.Target):
-    def exists(self):
-        try:
-            open('part-00079.gz', 'rw')
-            return True
-        except IOError:
-            return False
-
-    def open(self, mode):
-        return open('part-00079.gz', mode)
-
-class DummyTarget2(luigi.Target):
-    def exists(self):
-        try:
-            open('done', 'rw')
-            return True
-        except IOError:
-            return False
-
-    def open(self, mode):
-        return open('done', mode)
-
-class DummyTarget3(luigi.Target):
-    def exists(self):
-        try:
-            open('done_copying', 'rw')
-            return True
-        except IOError:
-            return False
-
-    def open(self, mode):
-        return open('done_copying', mode)
-
-class LocalLuigiTestInput(luigi.Task):
-    id = luigi.IntParameter()
-
-    def run(self):
-        print self.id
-        print "HERE WE ARE"
-
-    def output(self):
-        return DummyTarget()
-
-class LocalLuigiTestTask(luigi.Task):
-    def requires(self):
-        return VerticaEventLoadingTask(overwrite=True, schema='experimental', credentials='vertica_creds', interval='2015-07')
-
-    def run(self):
-        print "HELLO, WORLD!"
-        b = luigi.DateIntervalParameter('2015-07')
-        for d in luigi.DateIntervalParameter('2015-07-01/03').parse(b):
-            print str(type(d))
-            b = luigi.DateParameter(d)
-        self.output().open('w').write("DONE!")
-
-    def output(self):
-        luigi.DateIntervalParameter('2015-07-01/03').parse('2015-07-01-2015-07-03')
-        return DummyTarget2()
-
-
-class Dummy4(luigi.Task):
-    def output(self):
-        return get_target_from_url('/Users/jamesrowan/test_loader_folder/part-00024')
-        # return luigi.LocalTarget('/Users/jamesrowan/test_loader_folder/')
-        # print str(type(get_target_from_url('s3://edx-analytics-data/dev/warehouse/events-vertica/dt=2015-07-20/part-00023')))
-        # return get_target_from_url('s3://edx-analytics-data/dev/warehouse/events-vertica/dt=2015-07-20/part-00023')
 
 
 class VerticaEventLoadingTask(VerticaCopyTask):
@@ -95,20 +24,12 @@ class VerticaEventLoadingTask(VerticaCopyTask):
     @property
     def insert_source_task(self):
         """The previous task in the workflow is to clean the data for loading into Vertica."""
-        # return Dummy4()
         return(CleanForVerticaTask(date=self.run_date, remove_implicit=self.remove_implicit))
-        # for day in luigi.DateIntervalParameter().parse(self.run_date):
-        #     yield CleanForVerticaTask(date=day, remove_implict=self.remove_implict.value)
-
-    def requires(self):
-        reqs = super(VerticaEventLoadingTask, self).requires()
-        reqs['extra'] = LogConcatenator(run_date=self.run_date, remove_implicit=self.remove_implicit)
-        return reqs
 
     @property
     def table(self):
         """We use the table event_logs for this task."""
-        return "event_logs_test"
+        return "event_logs"
 
     def create_table(self, connection):
         """Overriden because we will create a flex table instead of a traditional table."""
@@ -162,80 +83,28 @@ class VerticaEventLoadingTask(VerticaCopyTask):
         """Overridden since the superclass method includes a time of insertion column we don't want in this table."""
         return None
 
-    # TODO: we are flattening maps right now, right?
-    # TODO: address the vertica copy not doing the problem, maybe?
     def copy_data_table_from_target(self, cursor):
-        """Overriden since we copy from gzip files and need to use the json parser."""
-
-        # This one causes 100 Java IOExceptions about "inability to stream" to be thrown, one for each part file
-        # with self.input()['insert_source'].open('r') as insert_source_file:
-        #     print "HELLO, WE OPENED IT!"
-        #     cursor.copy_stream("COPY {schema}.{table} FROM STDIN GZIP PARSER fjsonparser() NO COMMIT;"
-        #                        .format(schema=self.schema, table=self.table), insert_source_file)
-        #     print "NO ERRORS THROWN!"
-
-        # trying nongzipped locally
-        # with self.input()['insert_source'].open('r') as insert_source_file:
-
-        # insert_source_file = self.input()['insert_source'].open('r')
-
-        if hasattr(self.input()['insert_source'], 'format'):
-            print self.input()['insert_source'].format
-
-        print self.input()['insert_source'].fs
-        print self.input()['insert_source'].fs.listdir()
-
+        """Overriden since we need to use the json parser."""
         with self.input()['insert_source'].open('r') as insert_source_file:
-            print "HELLO, WE OPENED IT!"
-
-            # If we are using an overwrite
+            # If we are using an overwrite, the overwriting commands may cause the queue of messages
+            # from the database to have extra information, which will confuse the copy query.
             if self.overwrite:
                 cursor.flush_to_query_ready()
 
-            cursor.copy_stream("COPY {schema}.{table} FROM STDIN GZIP PARSER fjsonparser() NO COMMIT;"
+            cursor.copy_stream("COPY {schema}.{table} FROM STDIN PARSER fjsonparser() NO COMMIT;"
                                .format(schema=self.schema, table=self.table), insert_source_file)
-
-            # while True:
-            #     message = cursor.connection.read_message()
-            #
-            #     print message
-
-            print "NO ERRORS THROWN!"
-
-        print "closed"
-
-
-        # This one fails because gzip.open expects a string or buffer
-
-        # for place in self.input()['insert_source'].fs.listdir(self.input()['insert_source'].path):
-        #     print place
-        #     with gzip.open(get_target_from_url(url_path_join(self.input()['insert_source'].path, place), 'r')) as insert_source_file:
-        #         cursor.copy_file("COPY {schema}.{table} FROM STDIN PARSER fjsonparser() NO COMMIT;"
-        #                          .format(schema=self.schema, table=self.table), insert_source_file, decoder='utf-8')
-
-        # with gzip.open(self.input()['insert_source'], 'r') as insert_source_file:
-        #     cursor.copy_file("COPY {schema}.{table} FROM STDIN PARSER fjsonparser() NO COMMIT;"
-        #                      .format(schema=self.schema, table=self.table), insert_source_file, decoder='utf-8')
-
-
-class LogConcatenator(luigi.Task):
-    """
-    Concatenate the tracking log files from a single day into a single file.
-    """
-    run_date = luigi.DateParameter(default=datetime.datetime.utcnow().date())
-    remove_implicit = luigi.BooleanParameter(default=True)
-
-    def requires(self):
-        CleanForVerticaTask(date=self.run_date, remove_implicit=self.remove_implicit)
-
-    def run(self):
-        print self.input().fs.listdir('.')
 
 
 class VerticaEventLoadingWorkflow(VerticaCopyTaskMixin, luigi.WrapperTask):
-    """Workflow for encapsulating the Vertica event loading task and passing in parameters."""
+    """
+    Workflow for encapsulating the Vertica event loading task and passing in parameters.
+
+    Writes in events with the individual days' worth of events as the atomic level; each day is a separate task
+    and will succeed or fail separately; query the marker table to determine which ones succeed.
+    """
     interval = luigi.DateIntervalParameter()
     remove_implicit = luigi.BooleanParameter(default=True)
+    use_flex = luigi.BooleanParameter(default=True)
 
     def requires(self):
         # Add additional args for VerticaCopyMixin.
@@ -246,19 +115,6 @@ class VerticaEventLoadingWorkflow(VerticaCopyTaskMixin, luigi.WrapperTask):
         kwargs2.update(kwargs2)
 
         for day in luigi.DateIntervalParameter().parse(str(self.interval)):
-            task_needed = VerticaEventLoadingTask(run_date=day, remove_implicit=self.remove_implicit, **kwargs2)
-            print str(task_needed)
+            task_needed = VerticaEventLoadingTask(run_date=day, remove_implicit=self.remove_implicit,
+                                                  use_flex=self.use_flex, **kwargs2)
             yield task_needed
-
-        # yield (
-        #     VerticaEventLoadingTask(**kwargs2),
-        # )
-
-    # def run(self):
-    #     self.output().open('w').write("DONE!")
-    #
-    # def output(self):
-    #     return DummyTarget2()
-
-if __name__ == '__main__':
-    luigi.run(main_task_cls=VerticaEventLoadingWorkflow)
--- edx/analytics/tasks/tests/acceptance/test_events_to_warehouse.py
+++ edx/analytics/tasks/tests/acceptance/test_events_to_warehouse.py
@@ -1,3 +1,80 @@
 """
-End-to-end test of the
+End-to-end test of the preliminary event-loading infrastructure for loading event logs into Vertica.
 """
+import os
+import logging
+import datetime
+
+import pandas
+
+from edx.analytics.tasks.tests.acceptance import AcceptanceTestCase
+from edx.analytics.tasks.url import url_path_join
+
+
+log = logging.getLogger(__name__)
+
+
+class BaseEventsToWarehouseAcceptanceTest(AcceptanceTestCase):
+    """Base class for the end-to-end test of event loading tasks."""
+
+    INPUT_FILE = 'events_to_warehouse_acceptance_tracking.log'
+
+    def setUp(self):
+        super(BaseEventsToWarehouseAcceptanceTest, self).setUp()
+
+        assert 'oddjob_jar' in self.config
+
+        self.oddjob_jar = self.config['oddjob_jar']
+
+        self.upload_data()
+
+    def upload_data(self):
+        """Puts the test course catalog where the processing task would look for it, bypassing calling the actual API"""
+        src = os.path.join(self.data_dir, 'input', self.INPUT_FILE)
+        dst = url_path_join(self.warehouse_path, 'tracking-logs', self.INPUT_FILE)
+
+        # Upload mocked results of the API call
+        self.s3_client.put(src, dst)
+
+
+class EventsToFlexTableAcceptanceTest(BaseEventsToWarehouseAcceptanceTest):
+    """End-to-end test of pulling the event data into Vertica flex tables."""
+
+    def test_events_to_warehouse_flex_table(self):
+        """Tests the workflow for the course subjects, end to end."""
+
+        self.task.launch([
+            'VerticaEventLoadingWorkflow',
+            '--credentials', self.vertica.vertica_creds_url,
+            '--interval', '2014-04-12-2014-04-15',
+            '--use-flex', '--remove-implicit'
+        ])
+
+        self.validate_output()
+
+        # Drop the table afterwards so that the next test starts with a clean table.
+        with self.vertica.cursor() as cursor:
+            cursor.execute("DROP TABLE IF EXISTS {schema}.d_course_subjects".format(schema=self.vertica.schema_name))
+
+    def validate_output(self):
+        """Validates the output, comparing it to a csv of all the expected events in this name."""
+        with self.vertica.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM {schema}.event_logs;"
+                           .format(schema=self.vertica.schema_name))
+            total_count = cursor.fetchone()[0]
+
+            expected_total_count = 3
+            self.assertEqual(total_count, expected_total_count)
+
+            # Verify that all the subject data loaded into Vertica is correct.
+            expected_output_csv = os.path.join(self.data_dir, 'output', 'events_to_warehouse_acceptance_flex_events.csv')
+            expected = pandas.read_csv(expected_output_csv)
+
+            cursor.execute("SELECT * FROM {schema}.event_logs;".format(schema=self.vertica.schema_name))
+            events = cursor.fetchall()
+
+            print "OBSERVED:"
+            print events
+            print "EXPECTED:"
+
+            self.fail("Just for debugging")
