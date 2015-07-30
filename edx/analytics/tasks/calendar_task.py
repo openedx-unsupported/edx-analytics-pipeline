12660b2d0b8699cbf79db95a9d110d45959b8aef
jamesrowan <jrowan@edx.org>
2015-07-30 10:29:21 -0400

    reverted changes to calendar_task.py

--- edx/analytics/tasks/calendar_task.py
+++ edx/analytics/tasks/calendar_task.py
@@ -9,7 +9,7 @@ import luigi.configuration
 
 from edx.analytics.tasks.url import get_target_from_url, url_path_join
 from edx.analytics.tasks.util import Week
-from edx.analytics.tasks.util.hive import HiveTableTask, HivePartitionTask
+from edx.analytics.tasks.util.hive import HiveTableTask, HivePartition
 from edx.analytics.tasks.util.overwrite import OverwriteOutputMixin
 
 log = logging.getLogger(__name__)
@@ -64,7 +64,7 @@ class CalendarTask(CalendarDownstreamMixin, luigi.Task):
                 output_file.write('\t'.join([unicode(v).encode('utf8') for v in column_values]) + '\n')
 
 
-class CalendarTableTask(HiveTableTask):
+class CalendarTableTask(CalendarDownstreamMixin, HiveTableTask):
     """Ensure a hive table exists for the calendar so that we can perform joins."""
 
     @property
@@ -84,23 +84,12 @@ class CalendarTableTask(HiveTableTask):
             ('iso_weekday', 'INT'),
         ]
 
-
-class CalendarPartitionTask(CalendarDownstreamMixin, HivePartitionTask):
-
     @property
-    def hive_table_task(self):
-        return CalendarTableTask(
-            warehouse_path=self.warehouse_path,
-            overwrite=self.overwrite,
-        )
-
-    @property
-    def partition_value(self):
-        return self.interval.date_b.isoformat()
+    def partition(self):
+        return HivePartition('interval', str(self.interval))
 
     def requires(self):
-        yield self.hive_table_task
-        yield CalendarTask(
+        return CalendarTask(
             output_root=self.partition_location,
             interval=self.interval,
             overwrite=self.overwrite,
