c2e532c8c739da39592eda74c4315c94b67e1bf4
jamesrowan <jrowan@edx.org>
2015-07-30 15:12:45 -0400

    re-gzipped the tracking log

--- edx/analytics/tasks/tests/acceptance/test_events_to_warehouse.py
+++ edx/analytics/tasks/tests/acceptance/test_events_to_warehouse.py
@@ -17,7 +17,7 @@ log = logging.getLogger(__name__)
 class BaseEventsToWarehouseAcceptanceTest(AcceptanceTestCase):
     """Base class for the end-to-end test of event loading tasks."""
 
-    INPUT_FILE = 'events_to_warehouse_acceptance_tracking.log'
+    INPUT_FILE = 'events_to_warehouse_acceptance_tracking.log.gz'
 
     def setUp(self):
         super(BaseEventsToWarehouseAcceptanceTest, self).setUp()
