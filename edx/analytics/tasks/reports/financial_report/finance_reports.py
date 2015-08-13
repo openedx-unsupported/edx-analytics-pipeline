a1b3aae364e40f66e6d3a426d1e8ec293dda0b9b
John Baker <john@edx.org>
2015-08-13 07:30:10 -0400

    First commit of AN5614

---
+++ edx/analytics/tasks/reports/financial_report/finance_reports.py
@@ -0,0 +1,59 @@
+# import datetime
+import luigi
+import luigi.hdfs
+import luigi.date_interval
+
+from edx.analytics.tasks.reports.financial_report.ed_services_financial_report import BuildEdServicesReportTask
+
+# from edx.analytics.tasks.util.overwrite import OverwriteOutputMixin
+# from edx.analytics.tasks.util.hive import HiveTableFromQueryTask, HivePartition
+# from edx.analytics.tasks.reports.reconcile import ReconciledOrderTransactionTableTask
+# from edx.analytics.tasks.database_imports import (
+#     DatabaseImportMixin, ImportStudentCourseEnrollmentTask, ImportCourseModeTask
+# )
+
+class BuildFinancialReportsTask(luigi.WrapperTask):
+
+    def requires(self):
+        # kwargs = {
+        #     'num_mappers': self.num_mappers,
+        #     'verbose': self.verbose,
+        #     'import_date': self.import_date,
+        #     'overwrite': self.overwrite,
+        #     'mapreduce_engine': self.mapreduce_engine,
+        #     'n_reduce_tasks': self.n_reduce_tasks,
+        #     'transaction_source': self.transaction_source,
+        #     'order_source': self.order_source,
+        #     'interval': self.interval,
+        #     'pattern': self.pattern,
+        #     'output_root': self.partition_location,
+        # }
+
+
+    # Transaction Report Requires
+    # def requires(self):
+    #     return ReconcileOrdersAndTransactionsTask(
+    #         mapreduce_engine=self.mapreduce_engine,
+    #         n_reduce_tasks=self.n_reduce_tasks,
+    #         transaction_source=self.transaction_source,
+    #         order_source=self.order_source,
+    #         interval=self.interval,
+    #         pattern=self.pattern,
+    #         output_root=self.partition_location,
+    #         # overwrite=self.overwrite,
+    #     )
+
+        kwargs = {
+            'num_mappers': self.num_mappers,
+            'verbose': self.verbose,
+            'import_date': self.import_date,
+            'overwrite': self.overwrite,
+        }
+        yield (
+            BuildEdServicesReportTask(
+                destination=self.destination,
+                credentials=self.credentials,
+                database=self.database,
+                **kwargs
+            ),
+        )
