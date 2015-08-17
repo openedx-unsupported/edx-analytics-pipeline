5d099fa32254969f201f25bad25020685f054f69
John Baker <john@edx.org>
2015-08-17 10:03:47 -0400

    changes to input parameters for edservices report.

--- edx/analytics/tasks/reports/financial_report/finance_reports.py
+++ edx/analytics/tasks/reports/financial_report/finance_reports.py
@@ -55,7 +55,7 @@ class BuildFinancialReportsTask(
             'import_date': self.import_date,
             #'mapreduce_engine': self.mapreduce_engine,
             #'n_reduce_tasks': self.n_reduce_tasks,
-            # 'interval': self.interval,
+            'interval': self.interval,
             # 'pattern': self.pattern,
         }
 
