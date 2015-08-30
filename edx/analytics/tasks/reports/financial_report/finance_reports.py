6c422b2ad7e354a31e897c2e111d14103b2a9d39
John Baker <john@edx.org>
2015-08-30 10:41:43 -0400

    changes

--- edx/analytics/tasks/reports/financial_report/finance_reports.py
+++ edx/analytics/tasks/reports/financial_report/finance_reports.py
@@ -54,7 +54,6 @@ class BuildFinancialReportsTask(
             # 'verbose': self.verbose,
             # 'destination': self.destination,
             'interval_start': self.interval_start,
-            'interval': self.interval,
             # 'import_date': self.import_date,
             # 'interval_end': self.interval_end,
         }
