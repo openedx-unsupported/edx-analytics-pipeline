b3dae37047ab1bafa85cea2233024ab18c03bf64
John Baker <john@edx.org>
2015-08-31 07:05:11 -0400

    changes

--- edx/analytics/tasks/reports/financial_report/finance_reports.py
+++ edx/analytics/tasks/reports/financial_report/finance_reports.py
@@ -8,10 +8,11 @@
 # from edx.analytics.tasks.reports.financial_report.ed_services_financial_report import BuildEdServicesReportTask
 
 import luigi
+import logging
 import luigi.hdfs
 import luigi.date_interval
 import datetime
-from edx.analytics.tasks.reports.reconcile import ReconcileOrdersAndTransactionsDownstreamMixin
+from edx.analytics.tasks.reports.reconcile import ReconcileOrdersAndTransactionsDownstreamMixin, TransactionReportTask
 from edx.analytics.tasks.reports.financial_report.ed_services_financial_report import BuildEdServicesReportTask
 from edx.analytics.tasks.database_imports import (
     DatabaseImportMixin, ImportCourseModeTask, ImportStudentCourseEnrollmentTask
@@ -21,6 +22,7 @@ from edx.analytics.tasks.reports.orders_import import OrderTableTask
 from edx.analytics.tasks.reports.paypal import PaypalTransactionsByDayTask
 from edx.analytics.tasks.reports.cybersource import IntervalPullFromCybersourceTask
 
+log = logging.getLogger(__name__)
 
 class BuildFinancialReportsMixin(DatabaseImportMixin):
     #
@@ -90,9 +92,11 @@ class BuildFinancialReportsTask(
         # Ingest required data into HIVE needed to build the financial reports
         yield (
             # Import Order data
+            log.debug('Importing order data.'),
             OrderTableTask(),
 
             # Import payment provider data: PayPal
+            # log.debug('Importing PayPal data')
             PaypalTransactionsByDayTask(
                 start_date=self.start_date,
                 output_root=self.output_root,
@@ -100,13 +104,14 @@ class BuildFinancialReportsTask(
             ),
 
             # Import payment provider data: CyberSource - edx.org
+            # log.debug('Importing cybersource - edx.org data.')
             IntervalPullFromCybersourceTask(
                 merchant_id=self.merchant_id,
                 output_root=self.output_root,
                 **kwargs
             ),
 
-            # Import payment provider data: CyberSource - edx.org
+            # Import payment provider data: CyberSource - MIT Corp edX
             # IntervalPullFromCybersourceTask(
             #     merchant_id='mit_corp_edx',
             #     output_root=self.output_root,
@@ -114,13 +119,22 @@ class BuildFinancialReportsTask(
             # ),
 
             # Import Course Information: Mainly Course Mode & Suggested Prices
+            # log.debug('Importing course mode & suggested price data.')
             ImportCourseModeTask(),
+
             # Import Student Enrollment Information
+            # log.debug('Importing student course enrollment data.')
             ImportStudentCourseEnrollmentTask(),
+
             # Import Reconciled Orders and Transactions
+            # log.debug('Reconciling orders and transactions.')
             ReconciledOrderTransactionTableTask(),
 
-            BuildEdServicesReportTask(**kwargs)
+            # log.debug('Building ed services report.')
+            BuildEdServicesReportTask(**kwargs),
+
+            # log.debug('Building transaction report.')
+            TransactionReportTask(**kwargs)
         )
 
     # def run(self):
