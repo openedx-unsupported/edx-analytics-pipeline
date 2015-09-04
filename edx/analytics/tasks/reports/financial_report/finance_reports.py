45026bb4377e2d9ad8f3c8585d429ae5a7954b1b
Brian Wilson <brian@edx.org>
2015-09-04 17:36:28 -0400

    Update financial report workflow to include output of reports to internal warehouse.
    Includes Ed Services Report and Transaction-Orderitem tables.
    
    Overwrites contents in warehouse by default.

--- edx/analytics/tasks/reports/financial_report/finance_reports.py
+++ edx/analytics/tasks/reports/financial_report/finance_reports.py
@@ -1,23 +1,46 @@
+"""Provide entry-point for generating finance reports."""
 import luigi
-import luigi.hdfs
-import luigi.date_interval
 
-from edx.analytics.tasks.reports.reconcile import TransactionReportTask
-from edx.analytics.tasks.reports.financial_report.ed_services_financial_report import BuildEdServicesReportTask
-from edx.analytics.tasks.database_imports import DatabaseImportMixin
+from edx.analytics.tasks.reports.reconcile import (
+    TransactionReportTask, LoadInternalReportingOrderTransactionsToWarehouse
+)
+from edx.analytics.tasks.reports.financial_report.ed_services_financial_report import (
+    LoadInternalReportingEdServicesReportToWarehouse
+)
 from edx.analytics.tasks.mapreduce import MapReduceJobTaskMixin
+from edx.analytics.tasks.vertica_load import VerticaCopyTaskMixin
 
 
-class BuildFinancialReportsTask(DatabaseImportMixin, MapReduceJobTaskMixin, luigi.WrapperTask):
+class BuildFinancialReportsTask(MapReduceJobTaskMixin, VerticaCopyTaskMixin, luigi.WrapperTask):
+    """Provide entry-point for generating finance reports."""
+
+    # Instead of importing all of DatabaseImportMixin at this level, we just define
+    # what we need and are willing to pass through.  That way the use of "credentials"
+    # for the output of the report data is not conflicting.
+    import_date = luigi.DateParameter()
+
+    # Redefine the overwrite parameter to change its default to True.
+    # This will cause the reports to reload when loading into internal reporting.
+    overwrite = luigi.BooleanParameter(default=True)
 
     def requires(self):
         yield (
-            BuildEdServicesReportTask(
-                import_date=self.import_date,
-                n_reduce_tasks=self.n_reduce_tasks,
-            ),
             TransactionReportTask(
                 import_date=self.import_date,
                 n_reduce_tasks=self.n_reduce_tasks,
-            )
+            ),
+            LoadInternalReportingOrderTransactionsToWarehouse(
+                import_date=self.import_date,
+                n_reduce_tasks=self.n_reduce_tasks,
+                schema=self.schema,
+                credentials=self.credentials,
+                overwrite=self.overwrite,
+            ),
+            LoadInternalReportingEdServicesReportToWarehouse(
+                import_date=self.import_date,
+                n_reduce_tasks=self.n_reduce_tasks,
+                schema=self.schema,
+                credentials=self.credentials,
+                overwrite=self.overwrite,
+            ),
         )
