c16a42c21ea3a9e3a5665fe0b63991c8113bebd1
Gabe Mulley <gabe@edx.org>
2015-10-05 14:40:49 -0400

    fix distribute by course_id

--- edx/analytics/tasks/student_engagement.py
+++ edx/analytics/tasks/student_engagement.py
@@ -501,7 +501,7 @@ class StudentEngagementIndexTask(
             })
 
     def mapper(self, line):
-        yield (line.split('\t')[0].encode('utf8'), line)
+        yield (line.split('\t')[1].encode('utf8'), line)
 
     def reducer(self, _key, records):
         es = Elasticsearch(hosts=self.elasticsearch_host)
