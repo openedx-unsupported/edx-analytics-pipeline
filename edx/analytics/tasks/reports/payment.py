8876842105c3633127f6fdebfbd819b9b6940e9d
Tim Krones <tim@opencraft.com>
2016-03-25 16:26:28 +0100

    Ingest invoice transactions from ShoppingCart and Otto.

--- edx/analytics/tasks/reports/payment.py
+++ edx/analytics/tasks/reports/payment.py
@@ -2,6 +2,7 @@
 import luigi
 
 from edx.analytics.tasks.reports.cybersource import IntervalPullFromCybersourceTask
+from edx.analytics.tasks.reports.invoices import InvoiceTransactionsIntervalTask
 from edx.analytics.tasks.reports.paypal import PaypalTransactionsIntervalTask
 
 
@@ -12,16 +13,26 @@ class PaymentTask(luigi.WrapperTask):
         default_from_config={'section': 'payment', 'name': 'cybersource_merchant_ids'},
         is_list=True
     )
+    n_reduce_tasks = luigi.Parameter(
+        default=1,
+        significant=False,
+        description='Number of reducer tasks to use in upstream tasks.  Scale this to your cluster size.',
+    )
 
     def requires(self):
-        yield PaypalTransactionsIntervalTask(
-            interval_end=self.import_date
+        # Temporarily disable existing tasks to be able to run PaymentTask
+        # yield PaypalTransactionsIntervalTask(
+        #     interval_end=self.import_date
+        # )
+        # for merchant_id in self.cybersource_merchant_ids:
+        #     yield IntervalPullFromCybersourceTask(
+        #         interval_end=self.import_date,
+        #         merchant_id=merchant_id
+        #     )
+        yield InvoiceTransactionsIntervalTask(
+            interval_end=self.import_date,
+            n_reduce_tasks=self.n_reduce_tasks
         )
-        for merchant_id in self.cybersource_merchant_ids:
-            yield IntervalPullFromCybersourceTask(
-                interval_end=self.import_date,
-                merchant_id=merchant_id
-            )
 
     def output(self):
         return [task.output() for task in self.requires()]
