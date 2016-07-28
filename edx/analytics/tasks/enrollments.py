84516af9e8bd1534ee599fa28996be398fc7c764
Brian Wilson <brian@edx.org>
2016-07-28 12:11:25 -0400

    Add raw code from gabe/incremental-enrollment.

--- edx/analytics/tasks/enrollments.py
+++ edx/analytics/tasks/enrollments.py
@@ -5,14 +5,16 @@ import datetime
 
 import luigi
 import luigi.task
+import luigi.date_interval
 
+from edx.analytics.tasks.canonicalization import CanonicalizationTask
 from edx.analytics.tasks.database_imports import ImportAuthUserProfileTask
 from edx.analytics.tasks.mapreduce import MapReduceJobTaskMixin, MapReduceJobTask
-from edx.analytics.tasks.pathutil import EventLogSelectionDownstreamMixin, EventLogSelectionMixin
-from edx.analytics.tasks.url import get_target_from_url, url_path_join, ExternalURL
+from edx.analytics.tasks.pathutil import EventLogSelectionDownstreamMixin
+from edx.analytics.tasks.url import get_target_from_url, url_path_join
 from edx.analytics.tasks.util import eventlog, opaque_key_util
-from edx.analytics.tasks.util.hive import WarehouseMixin, HiveTableTask, HivePartition, HiveQueryToMysqlTask
-from edx.analytics.tasks.decorators import workflow_entry_point
+from edx.analytics.tasks.util.hive import WarehouseMixin, HiveTableTask, HivePartition, HiveQueryToMysqlTask, HivePartitionTask
+from edx.analytics.tasks.util.overwrite import OverwriteOutputMixin
 
 
 log = logging.getLogger(__name__)
@@ -21,18 +23,38 @@ ACTIVATED = 'edx.course.enrollment.activated'
 MODE_CHANGED = 'edx.course.enrollment.mode_changed'
 
 
-class CourseEnrollmentTask(EventLogSelectionMixin, MapReduceJobTask):
+class CourseEnrollmentTask(OverwriteOutputMixin, WarehouseMixin, MapReduceJobTask):
     """Produce a data set that shows which days each user was enrolled in each course."""
 
-    output_root = luigi.Parameter()
+    date = luigi.DateParameter(default=datetime.datetime.utcnow().date())
+    interval_start = luigi.DateParameter(
+        default_from_config={'section': 'enrollments', 'name': 'interval_start'},
+        significant=False,
+    )
 
-    enable_direct_output = True
+    def requires(self):
+        yield CanonicalizationTask(
+            date=self.date,
+            overwrite=self.overwrite,
+            warehouse_path=self.warehouse_path,
+        )
+        if self.date > self.interval_start:
+            yield CourseEnrollmentTask(
+                date=(self.date - datetime.timedelta(days=1)),
+                overwrite=self.overwrite,
+                warehouse_path=self.warehouse_path,
+                n_reduce_tasks=self.n_reduce_tasks,
+            )
 
     def mapper(self, line):
-        value = self.get_event_and_date_string(line)
-        if value is None:
-            return
-        event, _date_string = value
+        event = eventlog.parse_json_event(line)
+        if event is None:
+            try:
+                datestamp, course_id, user_id, enrolled_at_end, change_since_last_day, mode_at_end = line.split('\t')
+                yield (course_id, int(user_id)), ("state", int(enrolled_at_end), mode_at_end)
+                return
+            except ValueError:
+                return
 
         event_type = event.get('event_type')
         if event_type is None:
@@ -66,28 +88,18 @@ class CourseEnrollmentTask(EventLogSelectionMixin, MapReduceJobTask):
             log.error("encountered explicit enrollment event with no mode: %s", event)
             return
 
-        yield (course_id, user_id), (timestamp, event_type, mode)
+        yield (course_id, user_id), ("event", timestamp, event_type, mode)
 
     def reducer(self, key, values):
         """Emit records for each day the user was enrolled in the course."""
         course_id, user_id = key
 
-        event_stream_processor = DaysEnrolledForEvents(course_id, user_id, self.interval, values)
-        for day_enrolled_record in event_stream_processor.days_enrolled():
+        event_stream_processor = DaysEnrolledForEvents(course_id, user_id, self.date, values)
+        for day_enrolled_record in event_stream_processor.enrollments():
             yield day_enrolled_record
 
     def output(self):
-        return get_target_from_url(self.output_root)
-
-    def complete(self):
-        return get_target_from_url(url_path_join(self.output_root, '_SUCCESS')).exists()
-
-    def run(self):
-        output_target = self.output()
-        if not self.complete() and output_target.exists():
-            output_target.remove()
-
-        super(CourseEnrollmentTask, self).run()
+        return get_target_from_url(url_path_join(self.warehouse_path, 'course_enrollment', 'dt=' + self.date.isoformat()) + '/')
 
 
 class EnrollmentEvent(object):
@@ -98,6 +110,13 @@ class EnrollmentEvent(object):
         self.datestamp = eventlog.timestamp_to_datestamp(timestamp)
         self.event_type = event_type
         self.mode = mode
+
+
+class EnrollmentState(object):
+
+    def __init__(self, enrolled_at_end, mode_at_end):
+        self.enrolled_at_end = enrolled_at_end
+        self.mode_at_end = mode_at_end
 
 
 class DaysEnrolledForEvents(object):
@@ -161,138 +180,87 @@ class DaysEnrolledForEvents(object):
     UNENROLLED = 0
     MODE_UNKNOWN = 'unknown'
 
-    def __init__(self, course_id, user_id, interval, events):
+    def __init__(self, course_id, user_id, date, state_and_events):
         self.course_id = course_id
         self.user_id = user_id
-        self.interval = interval
+        self.date = date
 
-        self.sorted_events = sorted(events)
-        # After sorting, we can discard time information since we only care about date transitions.
+        self.sorted_events = sorted(state_and_events)
+        previous_state = None
+        for event in self.sorted_events:
+            if event[0] == 'state':
+                previous_state = EnrollmentState(
+                    enrolled_at_end=event[1],
+                    mode_at_end=event[2],
+                )
+
         self.sorted_events = [
-            EnrollmentEvent(timestamp, event_type, mode) for timestamp, event_type, mode in self.sorted_events
+            EnrollmentEvent(event[1], event[2], event[3]) for event in self.sorted_events
+            if event[0] == 'event'
         ]
-        # Since each event looks ahead to see the time of the next event, insert a dummy event at then end that
-        # indicates the end of the requested interval. If the user's last event is an enrollment activation event then
-        # they are assumed to be enrolled up until the end of the requested interval. Note that the mapper ensures that
-        # no events on or after date_b are included in the analyzed data set.
-        self.sorted_events.append(EnrollmentEvent(self.interval.date_b.isoformat(), None, None))  # pylint: disable=no-member
 
-        self.first_event = self.sorted_events[0]
+        if previous_state is None:
+            self.first_event = self.sorted_events[0]
 
-        # track the previous state in order to easily detect state changes between days.
-        if self.first_event.event_type == DEACTIVATED:
-            # First event was an unenrollment event, assume the user was enrolled before that moment in time.
-            log.warning('First event is an unenrollment for user %d in course %s on %s',
-                        self.user_id, self.course_id, self.first_event.datestamp)
-        elif self.first_event.event_type == MODE_CHANGED:
-            log.warning('First event is a mode change for user %d in course %s on %s',
-                        self.user_id, self.course_id, self.first_event.datestamp)
+            # track the previous state in order to easily detect state changes between days.
+            if self.first_event.event_type == DEACTIVATED:
+                # First event was an unenrollment event, assume the user was enrolled before that moment in time.
+                log.warning('First event is an unenrollment for user %d in course %s on %s',
+                            self.user_id, self.course_id, self.first_event.datestamp)
+            elif self.first_event.event_type == MODE_CHANGED:
+                log.warning('First event is a mode change for user %d in course %s on %s',
+                            self.user_id, self.course_id, self.first_event.datestamp)
 
-        # Before we start processing events, we can assume that their current state is the same as it has been for all
-        # time before the first event.
-        self.state = self.previous_state = self.UNENROLLED
-        self.mode = self.MODE_UNKNOWN
-
-    def days_enrolled(self):
-        """
-        A record is yielded for each day during which the user was enrolled in the course.
-
-        Yields:
-            tuple: An enrollment record for each day during which the user was enrolled in the course.
-
-        """
-        # The last element of the list is a placeholder indicating the end of the interval. Don't process it.
-        for index in range(len(self.sorted_events) - 1):
-            self.event = self.sorted_events[index]
-            self.next_event = self.sorted_events[index + 1]
-
-            self.change_state()
-
-            if self.event.datestamp != self.next_event.datestamp:
-                change_since_last_day = self.state - self.previous_state
-
-                # There may be a very wide gap between this event and the next event. If the user is currently
-                # enrolled, we can assume they continue to be enrolled at least until the next day we see an event.
-                # Emit records for each of those intermediary days. Since the end of the interval is represented by
-                # a dummy event at the end of the list of events, it will be represented by self.next_event when
-                # processing the last real event in the stream. This allows the records to be produced up to the end
-                # of the interval if the last known state was "ENROLLED".
-                for datestamp in self.all_dates_between(self.event.datestamp, self.next_event.datestamp):
-                    yield self.enrollment_record(
-                        datestamp,
-                        self.state,
-                        change_since_last_day if datestamp == self.event.datestamp else 0,
-                        self.mode
-                    )
-
-                self.previous_state = self.state
-
-    def all_dates_between(self, start_date_str, end_date_str):
-        """
-        All dates from the start date up to the end date.
-
-        Yields:
-            str: ISO 8601 datestamp for each date from the first date (inclusive) up to the end date (exclusive).
-
-        """
-        current_date = self.parse_date_string(start_date_str)
-        end_date = self.parse_date_string(end_date_str)
-
-        while current_date < end_date:
-            yield current_date.isoformat()
-            current_date += datetime.timedelta(days=1)
-
-    def parse_date_string(self, date_str):
-        """Efficiently parse an ISO 8601 date stamp into a datetime.date() object."""
-        date_parts = [int(p) for p in date_str.split('-')[:3]]
-        return datetime.date(*date_parts)
-
-    def enrollment_record(self, datestamp, enrolled_at_end, change_since_last_day, mode_at_end):
-        """A complete enrollment record."""
-        return (datestamp, self.course_id, self.user_id, enrolled_at_end, change_since_last_day, mode_at_end)
-
-    def change_state(self):
-        """Change state when appropriate.
-
-        Note that in spite of our best efforts some events might be lost, causing invalid state transitions.
-        """
-        self.mode = self.event.mode
-
-        if self.state == self.ENROLLED and self.event.event_type == DEACTIVATED:
+            # Before we start processing events, we can assume that their current state is the same as it has been for all
+            # time before the first event.
+            self.previous_state = EnrollmentState(0, self.MODE_UNKNOWN)
             self.state = self.UNENROLLED
-        elif self.state == self.UNENROLLED and self.event.event_type == ACTIVATED:
-            self.state = self.ENROLLED
-        elif self.event.event_type == MODE_CHANGED:
-            pass
+            self.mode = self.MODE_UNKNOWN
         else:
-            log.warning(
-                'No state change for %s event. User %d is already in the requested state for course %s on %s.',
-                self.event.event_type, self.user_id, self.course_id, self.event.datestamp
-            )
+            self.previous_state = previous_state
+            self.state = self.previous_state.enrolled_at_end
+            self.mode = previous_state.mode_at_end
+
+    def enrollments(self):
+        for event in self.sorted_events:
+            self.mode = event.mode
+
+            if self.state == self.ENROLLED and event.event_type == DEACTIVATED:
+                self.state = self.UNENROLLED
+            elif self.state == self.UNENROLLED and event.event_type == ACTIVATED:
+                self.state = self.ENROLLED
+            elif event.event_type == MODE_CHANGED:
+                pass
+            else:
+                log.warning(
+                    'No state change for %s event. User %d is already in the requested state for course %s on %s.',
+                    event.event_type, self.user_id, self.course_id, event.datestamp
+                )
+
+        if self.state == self.ENROLLED and self.previous_state.enrolled_at_end == 0:
+            change = 1
+        elif self.state == self.UNENROLLED and self.previous_state.enrolled_at_end == 1:
+            change = -1
+        else:
+            change = 0
+
+        yield (
+            self.date.isoformat(), self.course_id, self.user_id, self.state, change, self.mode
+        )
 
 
 class CourseEnrollmentTableDownstreamMixin(WarehouseMixin, EventLogSelectionDownstreamMixin, MapReduceJobTaskMixin):
     """All parameters needed to run the CourseEnrollmentTableTask task."""
 
     # Make the interval be optional:
-    interval = luigi.DateIntervalParameter(
-        default=None,
-        description='The range of dates to export logs for. '
-        'If not specified, `interval_start` and `interval_end` are used to construct the `interval`.',
-    )
+    interval = luigi.DateIntervalParameter(default=None)
 
     # Define optional parameters, to be used if 'interval' is not defined.
     interval_start = luigi.DateParameter(
-        config_path={'section': 'enrollments', 'name': 'interval_start'},
+        default_from_config={'section': 'enrollments', 'name': 'interval_start'},
         significant=False,
-        description='The start date to export logs for.  Ignored if `interval` is provided.',
     )
-    interval_end = luigi.DateParameter(
-        default=datetime.datetime.utcnow().date(),
-        significant=False,
-        description='The end date to export logs for.  Ignored if `interval` is provided. '
-        'Default is today, UTC.',
-    )
+    interval_end = luigi.DateParameter(default=datetime.datetime.utcnow().date(), significant=False)
 
     def __init__(self, *args, **kwargs):
         super(CourseEnrollmentTableDownstreamMixin, self).__init__(*args, **kwargs)
@@ -301,7 +269,7 @@ class CourseEnrollmentTableDownstreamMixin(WarehouseMixin, EventLogSelectionDown
             self.interval = luigi.date_interval.Custom(self.interval_start, self.interval_end)
 
 
-class CourseEnrollmentTableTask(CourseEnrollmentTableDownstreamMixin, HiveTableTask):
+class CourseEnrollmentTableTask(HiveTableTask):
     """Hive table that stores the set of users enrolled in each course over time."""
 
     @property
@@ -319,26 +287,29 @@ class CourseEnrollmentTableTask(CourseEnrollmentTableDownstreamMixin, HiveTableT
             ('mode', 'STRING'),
         ]
 
-    @property
-    def partition(self):
-        return HivePartition('dt', self.interval.date_b.isoformat())  # pylint: disable=no-member
 
-    def requires(self):
-        return CourseEnrollmentTask(
-            mapreduce_engine=self.mapreduce_engine,
-            n_reduce_tasks=self.n_reduce_tasks,
-            source=self.source,
-            interval=self.interval,
-            pattern=self.pattern,
-            output_root=self.partition_location,
+class CourseEnrollmentPartitionTask(CourseEnrollmentTableDownstreamMixin, HivePartitionTask):
+
+    @property
+    def hive_table_task(self):
+        return CourseEnrollmentTableTask(
+            warehouse_path=self.warehouse_path,
+            overwrite=self.overwrite,
         )
 
-
-class ExternalCourseEnrollmentTableTask(CourseEnrollmentTableTask):
+    @property
+    def partition_value(self):
+        return self.date.isoformat()
 
     def requires(self):
-        yield ExternalURL(
-            url=url_path_join(self.warehouse_path, 'course_enrollment', self.partition.path_spec) + '/'
+        yield self.hive_table_task
+        yield CourseEnrollmentTask(
+            mapreduce_engine=self.mapreduce_engine,
+            n_reduce_tasks=self.n_reduce_tasks,
+            warehouse_path=self.warehouse_path,
+            date=self.interval_end,
+            interval_start=self.interval_start,
+            overwrite=self.overwrite,
         )
 
 
@@ -355,19 +326,18 @@ class EnrollmentTask(CourseEnrollmentTableDownstreamMixin, HiveQueryToMysqlTask)
         ]
 
     @property
-    def partition(self):
-        return HivePartition('dt', self.interval.date_b.isoformat())  # pylint: disable=no-member
+    def partition_value(self):
+        return self.interval.date_b.isoformat()
 
     @property
     def required_table_tasks(self):
         yield (
-            CourseEnrollmentTableTask(
+            CourseEnrollmentPartitionTask(
                 mapreduce_engine=self.mapreduce_engine,
                 n_reduce_tasks=self.n_reduce_tasks,
-                source=self.source,
                 interval=self.interval,
-                pattern=self.pattern,
                 warehouse_path=self.warehouse_path,
+                overwrite=self.hive_overwrite,
             ),
             ImportAuthUserProfileTask()
         )
@@ -569,16 +539,13 @@ class EnrollmentDailyTask(EnrollmentTask):
         ]
 
 
-@workflow_entry_point
 class ImportEnrollmentsIntoMysql(CourseEnrollmentTableDownstreamMixin, luigi.WrapperTask):
     """Import all breakdowns of enrollment into MySQL"""
 
     def requires(self):
         kwargs = {
             'n_reduce_tasks': self.n_reduce_tasks,
-            'source': self.source,
             'interval': self.interval,
-            'pattern': self.pattern,
             'warehouse_path': self.warehouse_path,
         }
         yield (
