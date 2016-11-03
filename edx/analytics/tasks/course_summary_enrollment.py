14cf15fd9d571702d941fd4a7d37ef18f51acc76
Dennis Jen <djen@edx.org>
2016-11-03 10:01:53 -0400

    added missing comma in query

--- edx/analytics/tasks/course_summary_enrollment.py
+++ edx/analytics/tasks/course_summary_enrollment.py
@@ -166,7 +166,7 @@ class CourseSummaryEnrollmentPartitionTask(CourseSummaryEnrollmentDownstreamMixi
                 course.availability,
                 enrollment_end.mode,
                 enrollment_end.count,
-                (enrollment_end.count - COALESCE(enrollment_start.count, 0)) as count_change_7_days
+                (enrollment_end.count - COALESCE(enrollment_start.count, 0)) as count_change_7_days,
                 enrollment_end.cumulative_count
             FROM course_enrollment_mode_daily as enrollment_end
             LEFT JOIN course_enrollment_mode_daily as enrollment_start
