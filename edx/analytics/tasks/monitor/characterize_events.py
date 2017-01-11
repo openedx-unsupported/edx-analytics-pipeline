a680a18ce9692257da82a2d6cd95fba60318f4d1
Hassan Javeed <hassan@edx.org>
2017-01-11 20:56:33 +0500

    check metadata

--- edx/analytics/tasks/monitor/characterize_events.py
+++ edx/analytics/tasks/monitor/characterize_events.py
@@ -37,7 +37,7 @@ class ListS3FilesWithDateTask(OverwriteOutputMixin, luigi.Task):
                 key_url = url_path_join('s3:////', bucket.name, key.name)
                 last_modified = key.last_modified
                 metadata = key.metadata
-                values = [key_url, last_modified, metadata]
+                values = [key_url, last_modified, json.dumps(metadata)]
 
                 if metadata:
                     match = re.match('.*?mtime:(?P<mtime>\d{10}).*?ctime:(?P<ctime>\d{10})', metadata.get('s3cmd-attrs', ''))
