85301c459bbafda75e0ab6500c2f08887a118ef0
Hassan Javeed <hassan@edx.org>
2017-01-12 19:29:59 +0500

    Added events upload time analysis task.

--- edx/analytics/tasks/monitor/characterize_events.py
+++ edx/analytics/tasks/monitor/characterize_events.py
@@ -2,8 +2,10 @@ import luigi
 import os
 import logging
 import re
+import datetime
 
 from boto.s3.connection import S3Connection
+from dateutil import parser
 
 from edx.analytics.tasks.common.mapreduce import MapReduceJobTask
 from edx.analytics.tasks.util.overwrite import OverwriteOutputMixin
@@ -38,9 +40,58 @@ class ListS3FilesWithDateTask(OverwriteOutputMixin, luigi.Task):
                 last_modified = key.last_modified
                 output_file.write('\t'.join((key_url, last_modified)))
                 output_file.write('\n')
-#
-#
-# class CharacterizeEventsTask(OverwriteOutputMixin, WarehouseMixin, EventLogSelectionMixin, MapReduceJobTask):
-#
-#     def init_local(self):
-#         super(CharacterizeEventsTask, self).init_local()
+
+
+class CharacterizeEventsTask(OverwriteOutputMixin, EventLogSelectionMixin, MapReduceJobTask):
+
+    output_root = luigi.Parameter()
+
+    bucket = luigi.Parameter()
+
+    key_prefix = luigi.Parameter(default='logs/tracking/')
+
+
+    url_to_timestamp_map = {}
+
+    def requires_local(self):
+        return ListS3FilesWithDateTask(
+            bucket=self.bucket,
+            key_prefix=self.key_prefix,
+            output_root=self.output_root,
+        )
+
+    def init_local(self):
+        super(CharacterizeEventsTask, self).init_local()
+        with self.input_local().open() as f_in:
+            lines = f_in.readlines()
+            for line in lines:
+                file_url, timestamp = line.split()
+                self.url_to_timestamp_map[file_url] = timestamp
+
+    def mapper(self, line):
+        input_file = os.environ['mapreduce_map_input_file']
+
+        value = self.get_event_and_date_string(line)
+        if value is None:
+            return
+        event, event_date = value
+
+        yield event_date, self.url_to_timestamp_map[input_file]
+
+    def reducer(self, key, values):
+        date_string = key
+        event_date = parser.parse(date_string)
+        event_cutoff_time = (event_date + datetime.timedelta(days=1)).replace(hour=04, minute=0, second=0, microseconds=0)
+        events_before_cutoff = 0
+        events_after_cutoff = 0
+        for value in values:
+            event_upload_time = parser.parse(value)
+            if event_upload_time <= event_cutoff_time:
+                events_before_cutoff += 1
+            elif event_upload_time > event_cutoff_time:
+                events_after_cutoff += 1
+
+        yield date_string, events_before_cutoff, events_after_cutoff
+
+    def output(self):
+        return get_target_from_url(url_path_join(self.output_root, 'characterize_events/'))
