4e2967b2110654aa3ce85ffb37932c8aad8aff19
Brian Wilson <brian@edx.org>
2017-03-14 12:57:47 -0400

    Comment out video counters.
    It seems that having too many counter calls causes the App Manager to die.  Twice.
    Rather than try to tune the AM, we are just commenting out the calls in the short term.

--- edx/analytics/tasks/common/pathutil.py
+++ edx/analytics/tasks/common/pathutil.py
@@ -290,7 +290,7 @@ class EventLogSelectionMixin(EventLogSelectionDownstreamMixin):
         date_string = event_time.split("T")[0]
 
         if date_string < self.lower_bound_date_string or date_string >= self.upper_bound_date_string:
-            self.incr_counter('Event', 'Discard Outside Date Interval', 1)
+            ## self.incr_counter('Event', 'Discard Outside Date Interval', 1)
             return None
 
         return event, date_string
