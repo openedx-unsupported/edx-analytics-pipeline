96221606d96c3ed97ef24fb6e07e55ecdee8968b
Hassan Javeed <hassan@edx.org>
2017-05-02 17:42:00 +0500

    test without vertica

--- edx/analytics/tasks/tests/acceptance/test_financial_reports.py
+++ edx/analytics/tasks/tests/acceptance/test_financial_reports.py
@@ -45,42 +45,6 @@ class FinancialReportsAcceptanceTest(AcceptanceTestCase):
         for filename in os.listdir(sql_fixture_base_url):
             self.execute_sql_fixture_file(url_path_join(sql_fixture_base_url, filename), database=database)
 
-    @when_vertica_available
-    def test_end_to_end(self):
-        self.task.launch([
-            'BuildFinancialReportsTask',
-            '--import-date', self.UPPER_BOUND_DATE,
-            '--n-reduce-tasks', str(self.NUM_REDUCERS),
-        ])
-
-        final_output_task = LoadInternalReportingOrderTransactionsToWarehouse(import_date=self.UPPER_BOUND_DATE)
-        columns = [x[0] for x in final_output_task.columns]
-
-        with self.vertica.cursor() as cursor:
-            expected_output_csv = os.path.join(self.data_dir, 'output', 'expected_financial_report.csv')
-            expected = pandas.read_csv(expected_output_csv, parse_dates=True)
-
-            cursor.execute("SELECT {columns} FROM {schema}.f_orderitem_transactions".format(
-                columns=','.join(columns),
-                schema=self.vertica.schema_name
-            ))
-            response = cursor.fetchall()
-            f_orderitem_transactions = pandas.DataFrame(response, columns=columns)
-
-            try:  # A ValueError will be thrown if the column names don't match or the two data frames are not square.
-                self.assertTrue(all(f_orderitem_transactions == expected))
-            except ValueError:
-                buf = StringIO()
-                f_orderitem_transactions.to_csv(buf)
-                print 'Actual:'
-                print buf.getvalue()
-                buf.seek(0)
-                expected.to_csv(buf)
-                print 'Expected:'
-                print buf.getvalue()
-                self.fail("Expected and returned data frames have different shapes or labels.")
-
-    @when_vertica_not_available
     def test_end_to_end_without_vertica(self):
         # Similar to test_end_to_end but it excludes the vertica part and it checks data values,
         # not just data shape.
