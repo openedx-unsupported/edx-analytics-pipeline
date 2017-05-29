e25f9e5008478c6fa9f36334c586f965e0f63c0f
Hassan Javeed <hassan@edx.org>
2017-05-29 18:23:55 +0500

    fix path

--- edx/analytics/tasks/common/bigquery_load.py
+++ edx/analytics/tasks/common/bigquery_load.py
@@ -157,7 +157,7 @@ class BigQueryLoadTask(OverwriteOutputMixin, luigi.Task):
 
         source_path = self.input()['source'].path
         parsed_url = urlparse.urlparse(source_path)
-        destination_path = url_path_join('gs:////', parsed_url.netloc, parsed_url.path)
+        destination_path = url_path_join('gs://{}'.format(parsed_url.netloc), parsed_url.path)
         log.debug(" ".join(['gsutil', '-m', 'rsync', source_path, destination_path]))
         return_code = subprocess.call(['gsutil', '-m', 'rsync', source_path, destination_path])
 
