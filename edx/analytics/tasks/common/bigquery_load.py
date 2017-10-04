940fd7cb8539cd2a2729b6d547fd500dce07abb2
Brian Wilson <brian@edx.org>
2017-10-04 16:42:44 -0400

    Add support for JSON event output.
    It coexists with regular event output, and is controlled by
    an optional parameter.  By default it runs with
    event_record_type equal to 'EventRecord', but can
    be overridden by running with --event-record-type 'JsonEventRecord'.
    
    Includes bug fix to timestamp handling:  Add validation screening dates < 1900.
    
    Also includes support for event loading to BigQuery, by adding
    support for partitioning to bigquery_load.
    
    * Use records for warehouse loading where defined.
    * Check bigquery availability in load code.
    
    Add support for loading to S3 by interval or by date.  PerDate loading
    checks whether the data already exists, which is good for incremental runs.
    Bulk loading just runs over an interval, and assumes the data isn't already
    present on S3.  This is better for processing many days more efficiently.
    
    To address issue with loading into BigQuery, null characters in column values
    are encoded as the string '\0'.

--- edx/analytics/tasks/common/bigquery_load.py
+++ edx/analytics/tasks/common/bigquery_load.py
@@ -9,16 +9,22 @@ import urlparse
 
 import luigi
 
-from google.cloud import bigquery
-from google.oauth2 import service_account
-from google.cloud.exceptions import NotFound
-
 from edx.analytics.tasks.util.url import ExternalURL, url_path_join
 from edx.analytics.tasks.util.overwrite import OverwriteOutputMixin
 from edx.analytics.tasks.util.hive import WarehouseMixin
 
-
 log = logging.getLogger(__name__)
+
+try:
+    from google.cloud import bigquery
+    from google.oauth2 import service_account
+    from google.cloud.exceptions import NotFound
+    bigquery_available = True  # pylint: disable=invalid-name
+except ImportError:
+    log.warn('Unable to import Bigquery libraries')
+    # On hadoop slave nodes we don't have bigquery libraries installed,
+    # so just fail noisily if we attempt to use these libraries there.
+    bigquery_available = False  # pylint: disable=invalid-name
 
 
 RETRY_LIMIT = 500
@@ -76,6 +82,11 @@ class BigQueryTarget(luigi.Target):
             tmp.close()
             os.unlink(tmp.name)
 
+    def marker_table_exists(self):
+        dataset = self.client.dataset(self.dataset_id)
+        table = dataset.table('table_updates')
+        return table.exists()
+
     def create_marker_table(self):
         marker_table_schema = [
             bigquery.SchemaField('update_id', 'STRING'),
@@ -87,10 +98,36 @@ class BigQueryTarget(luigi.Target):
         if not table.exists():
             table.create()
 
+    def clear_marker_table(self):
+        if not self.marker_table_exists():
+            return
+
+        query_string = "DELETE {dataset}.table_updates WHERE target_table='{dataset}.{table}'".format(
+            dataset=self.dataset_id, table=self.table
+        )
+        query = self.client.run_sync_query(query_string)
+        query.use_legacy_sql = False
+        query.run()
+
+    def clear_marker_table_entry(self):
+        if not self.marker_table_exists():
+            return
+
+        query_string = "DELETE {dataset}.table_updates WHERE update_id='{update_id}' AND target_table='{dataset}.{table}'".format(
+            dataset=self.dataset_id, update_id=self.update_id, table=self.table
+        )
+        query = self.client.run_sync_query(query_string)
+        query.use_legacy_sql = False
+        query.run()
+
     def exists(self):
-        query_string = "SELECT 1 FROM {dataset}.table_updates where update_id = '{update_id}'".format(
+        if not self.marker_table_exists():
+            return False
+
+        query_string = "SELECT 1 FROM {dataset}.table_updates WHERE update_id='{update_id}' AND target_table='{dataset}.{table}'".format(
             dataset=self.dataset_id,
             update_id=self.update_id,
+            table=self.table,
         )
         log.debug(query_string)
 
@@ -103,11 +140,20 @@ class BigQueryTarget(luigi.Target):
 
         return len(query.rows) == 1
 
-
-class BigQueryLoadTask(OverwriteOutputMixin, luigi.Task):
+class BigQueryLoadDownstreamMixin(OverwriteOutputMixin):
 
     dataset_id = luigi.Parameter()
     credentials = luigi.Parameter()
+    max_bad_records = luigi.IntParameter(
+        default=0, description="Number of bad records ignored by BigQuery before failing a load job."
+    )
+
+
+class BigQueryLoadTask(BigQueryLoadDownstreamMixin, luigi.Task):
+
+    # Regardless whether loading only a partition or an entire table,
+    # we still need a date to use to mark the table.
+    date = luigi.DateParameter()
 
     output_target = None
     required_tasks = None
@@ -133,6 +179,19 @@ class BigQueryLoadTask(OverwriteOutputMixin, luigi.Task):
         raise NotImplementedError
 
     @property
+    def table_description(self):
+        return ''
+
+    @property
+    def table_friendly_name(self):
+        return ''
+
+    @property
+    def partitioning_type(self):
+        """Set to 'DAY' in order to partition by day.  Default is to not partition at all."""
+        return None
+
+    @property
     def field_delimiter(self):
         return "\t"
 
@@ -153,37 +212,46 @@ class BigQueryLoadTask(OverwriteOutputMixin, luigi.Task):
         dataset = client.dataset(self.dataset_id)
         table = dataset.table(self.table, self.schema)
         if not table.exists():
+            if self.partitioning_type:
+                table.partitioning_type = self.partitioning_type
+            if self.table_description:
+                table.description = self.table_description
+            if self.table_friendly_name:
+                table.friendly_name = self.table_friendly_name
             table.create()
-
-    def init_touch(self, client):
-        if self.overwrite:
-            query = client.run_sync_query("delete {dataset}.table_updates where target_table='{dataset}.{table}'".format(
-                dataset=self.dataset_id, table=self.table
-            ))
-            query.use_legacy_sql = False
-            query.run()
 
     def init_copy(self, client):
         self.attempted_removal = True
         if self.overwrite:
             dataset = client.dataset(self.dataset_id)
             table = dataset.table(self.table)
-            if table.exists():
-                table.delete()
+            if self.partitioning_type:
+                # Delete only the specific partition, and clear the marker only for the partition.
+                # Note that there is no partition.exists() functionality that is useful.  Instead,
+                # it returns table.exists().  Likewise, partition.delete() is a no-op if the actual
+                # partition doesn't exist.  So if the table exists, we just try to delete the partition.
+                if table.exists():
+                    partition = self._get_table_partition(dataset, table)
+                    partition.delete()
+                self.output().clear_marker_table_entry()
+            else:
+                # Delete the entire table and all markers related to the table.
+                if table.exists():
+                    table.delete()
+                self.output().clear_marker_table()
 
-    def run(self):
-        client = self.output().client
-        self.create_dataset(client)
-        self.init_copy(client)
-        self.create_table(client)
-
-        dataset = client.dataset(self.dataset_id)
-        table = dataset.table(self.table, self.schema)
-
-        source_path = self.input()['source'].path
+    def _get_destination_from_source(self, source_path):
         parsed_url = urlparse.urlparse(source_path)
         destination_path = url_path_join('gs://{}'.format(parsed_url.netloc), parsed_url.path)
+        return destination_path
 
+    def _get_table_partition(self, dataset, table):
+        date_string = self.date.isoformat()
+        stripped_date = date_string.replace('-', '')
+        partition_name = '{}${}'.format(table.name, stripped_date)
+        return dataset.table(partition_name)
+
+    def _copy_data_to_gs(self, source_path, destination_path):
         if self.is_file(source_path):
             return_code = subprocess.call(['gsutil', 'cp', source_path, destination_path])
         else:
@@ -196,36 +264,63 @@ class BigQueryLoadTask(OverwriteOutputMixin, luigi.Task):
                 destination=destination_path,
             ))
 
+    def _get_load_url_from_destination(self, destination_path):
         if self.is_file(destination_path):
-            load_uri = destination_path
+            return destination_path
         else:
-            load_uri = url_path_join(destination_path, '*')
+            return url_path_join(destination_path, '*')
 
-        job = client.load_table_from_storage(
-            'load_{table}_{timestamp}'.format(table=self.table, timestamp=int(time.time())),
-            table,
-            load_uri
-        )
+    def _run_load_table_job(self, client, job_id, table, load_uri):
+        job = client.load_table_from_storage(job_id, table, load_uri)
         job.field_delimiter = self.field_delimiter
         job.quote_character = self.quote_character
         job.null_marker = self.null_marker
-
+        if self.max_bad_records > 0:
+            job.max_bad_records = self.max_bad_records
         log.debug("Starting BigQuery Load job.")
         job.begin()
-        wait_for_job(job)
-        try:
-            log.debug(
-                "Load job started: %s ended: %s input_files: %s output_rows: %s output_bytes: %s",
-                job.started,
-                job.ended,
-                job.input_files,
-                job.output_rows,
-                job.output_bytes,
-            )
-        except KeyError:
-            log.debug("Load job started: %s ended: %s No load stats.", job.started, job.ended)
+        wait_for_job(job, check_error_result=False)
 
-        self.init_touch(client)
+        try:
+            log.debug(" Load job started: %s ended: %s input_files: %s output_rows: %s output_bytes: %s",
+                      job.started, job.ended, job.input_files, job.output_rows, job.output_bytes)
+        except KeyError as keyerr:
+            log.debug(" Load job started: %s ended: %s No load stats.", job.started, job.ended)
+
+        if job.error_result:
+            for error in job.errors:
+                log.debug("   Load error: %s", error)
+            raise RuntimeError(job.errors)
+        else:
+            log.debug("   No errors encountered!")
+
+    def run(self):
+        self.check_bigquery_availability()
+
+        client = self.output().client
+        self.create_dataset(client)
+        self.init_copy(client)
+        self.create_table(client)
+
+        dataset = client.dataset(self.dataset_id)
+        table = dataset.table(self.table, self.schema)
+
+        source_path = self.input()['source'].path
+        destination_path = self._get_destination_from_source(source_path)
+        self._copy_data_to_gs(source_path, destination_path)
+        load_uri = self._get_load_url_from_destination(destination_path)
+
+        if self.partitioning_type:
+            partition = self._get_table_partition(dataset, table)
+            partition.partitioning_type = self.partitioning_type
+            job_id = 'load_{table}_{date_string}_{timestamp}'.format(
+                table=self.table, date_string=self.date.isoformat(), timestamp=int(time.time())
+            )
+            self._run_load_table_job(client, job_id, partition, load_uri)
+        else:
+            job_id = 'load_{table}_{timestamp}'.format(table=self.table, timestamp=int(time.time()))
+            self._run_load_table_job(client, job_id, table, load_uri)
+
         self.output().touch()
 
     def output(self):
@@ -248,3 +343,7 @@ class BigQueryLoadTask(OverwriteOutputMixin, luigi.Task):
         else:
             return False
 
+    def check_bigquery_availability(self):
+        """Call to ensure fast failure if this machine doesn't have the Bigquery libraries available."""
+        if not bigquery_available:
+            raise ImportError('Bigquery library not available')
