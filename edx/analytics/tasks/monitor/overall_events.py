695b58ae92669c112a5a7a7fda5d23519f44cad4
Brian Wilson <brian@edx.org>
2017-12-27 15:46:17 -0500

    Add test task.

--- edx/analytics/tasks/monitor/overall_events.py
+++ edx/analytics/tasks/monitor/overall_events.py
@@ -34,3 +34,41 @@ class TotalEventsDailyTask(EventLogSelectionMixin, MapReduceJobTask):
 
     def output(self):
         return get_target_from_url(self.output_root)
+
+# TAKE THIS OVER TO TEST USE OF SPARK:
+
+# modified from the Luigi example code, which is rather old.
+
+from luigi.contrib.spark import SparkSubmitTask, PySparkTask
+
+
+class InlinePySparkWordCount(PySparkTask):
+    """
+    This task runs a :py:class:`luigi.contrib.spark.PySparkTask` task
+    over the target data in :py:meth:`wordcount.input` (a file in S3) and
+    writes the result into its :py:meth:`wordcount.output` target (a file in S3).
+    This class uses :py:meth:`luigi.contrib.spark.PySparkTask.main`.
+    Example luigi configuration::
+        [spark]
+        spark-submit: /usr/local/spark/bin/spark-submit
+        master: spark://spark.example.org:7077
+        # py-packages: numpy, pandas
+    """
+    driver_memory = '2g'
+    executor_memory = '3g'
+
+    input_file = luigi.Parameter()
+    output_file = luigi.Parameter()
+
+    def input(self):
+        return get_target_from_url(self.input_file)
+
+    def output(self):
+        return get_target_from_url(self.output_file)
+
+    def main(self, sc, *args):
+        sc.textFile(self.input().path) \
+          .flatMap(lambda line: line.split()) \
+          .map(lambda word: (word, 1)) \
+          .reduceByKey(lambda a, b: a + b) \
+          .saveAsTextFile(self.output().path)
