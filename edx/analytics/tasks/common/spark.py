7097141493fadd4b58f6266b226985b3dd46f328
rao-abdul-mannan <raoabdulmannan@gmail.com>
2018-01-18 00:31:05 +0500

    internal imports loaded via spark context

--- edx/analytics/tasks/common/spark.py
+++ edx/analytics/tasks/common/spark.py
@@ -101,6 +101,20 @@ class SparkJobTask(OverwriteOutputMixin, PySparkTask):
         """
         raise NotImplementedError
 
+    def _load_external_dependency_on_cluster(self):
+        """creates a zip of analytics dir and loads it on spark worker nodes"""
+        # TODO: remove zipfile after loading on cluster completes
+        import os
+        import tempfile
+        import shutil
+        import edx.analytics as import_dir_path
+        package_name = 'edx_analytics_tasks'
+        tmp_dir = tempfile.mkdtemp()
+        # archive_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        archive_dir = import_dir_path.__path__[0]  # this is more reasonable than using path with __file__
+        zipfile_path = shutil.make_archive(os.path.join(tmp_dir, package_name), 'zip', archive_dir)
+        self._spark_context.addPyFile(zipfile_path)
+
     def run(self):
         self.remove_output_on_overwrite()
         super(SparkJobTask, self).run()
@@ -108,3 +122,76 @@ class SparkJobTask(OverwriteOutputMixin, PySparkTask):
     def main(self, sc, *args):
         self.init_spark(sc)
         self.spark_job()
+
+
+def get_event_predicate_labels(event):
+    """Creates labels by applying hardcoded predicates to a single event."""
+    # We only want the explicit event, not the implicit form.
+    # return 'test'
+    event_type = event['event_type']
+    event_source = event['event_source']
+
+    labels = PredicateLabels.ACTIVE_LABEL
+
+    # task & enrollment events are filtered out by spark later as it speeds up due to less # of records
+
+    if event_source == 'server':
+        if event_type == 'problem_check':
+            labels += ',' + PredicateLabels.PROBLEM_LABEL
+
+        if event_type.startswith('edx.forum.') and event_type.endswith('.created'):
+            labels += ',' + PredicateLabels.POST_FORUM_LABEL
+
+    if event_source in ('browser', 'mobile'):
+        if event_type == 'play_video':
+            labels += ',' + PredicateLabels.PLAY_VIDEO_LABEL
+
+    return labels
+
+
+def get_key_value_from_event(event, key, default_value=None):
+    """
+    Get value from event dict by key
+    Pyspark does not support dict.get() method, so this approach seems reasonable
+    """
+    try:
+        default_value = event[key]
+    except KeyError:
+        pass
+    return default_value
+
+
+def get_course_id(event, from_url=False):
+    """Gets course_id from event's data."""
+
+    # Get the event data:
+    event_context = get_key_value_from_event(event, 'context')
+    if event_context is None:
+        # Assume it's old, and not worth logging...
+        return ''
+
+    # Get the course_id from the data, and validate.
+    course_id = opaque_key_util.normalize_course_id(get_key_value_from_event(event_context, 'course_id', ''))
+    if course_id:
+        if opaque_key_util.is_valid_course_id(course_id):
+            return course_id
+        else:
+            return ''  # we'll filter out empty course since string is expected
+
+    # Try to get the course_id from the URLs in `event_type` (for implicit
+    # server events) and `page` (for browser events).
+    if from_url:
+        source = get_key_value_from_event(event, 'event_source')
+
+        if source == 'server':
+            url = get_key_value_from_event(event, 'event_type', '')
+        elif source == 'browser':
+            url = get_key_value_from_event(event, 'page', '')
+        else:
+            url = ''
+
+        course_key = opaque_key_util.get_course_key_from_url(url)
+        if course_key:
+            return unicode(course_key)
+
+    return ''
--- edx/analytics/tasks/insights/user_activity.py
+++ edx/analytics/tasks/insights/user_activity.py
@@ -21,81 +21,9 @@ from edx.analytics.tasks.util.overwrite import OverwriteOutputMixin
 import edx.analytics.tasks.util.opaque_key_util as opaque_key_util
 from edx.analytics.tasks.util.url import get_target_from_url, url_path_join
 from edx.analytics.tasks.util.weekly_interval import WeeklyIntervalMixin
-
 log = logging.getLogger(__name__)
 
 
-def get_event_predicate_labels(event):
-    """Creates labels by applying hardcoded predicates to a single event."""
-    # We only want the explicit event, not the implicit form.
-    # return 'test'
-    event_type = event['event_type']
-    event_source = event['event_source']
-
-    labels = PredicateLabels.ACTIVE_LABEL
-
-    # task & enrollment events are filtered out by spark later as it speeds up due to less # of records
-
-    if event_source == 'server':
-        if event_type == 'problem_check':
-            labels += ',' + PredicateLabels.PROBLEM_LABEL
-
-        if event_type.startswith('edx.forum.') and event_type.endswith('.created'):
-            labels += ',' + PredicateLabels.POST_FORUM_LABEL
-
-    if event_source in ('browser', 'mobile'):
-        if event_type == 'play_video':
-            labels += ',' + PredicateLabels.PLAY_VIDEO_LABEL
-
-    return labels
-
-
-def get_key_value_from_event(event, key, default_value=None):
-    """
-    Get value from event dict by key
-    Pyspark does not support dict.get() method, so this approach seems reasonable
-    """
-    try:
-        default_value = event[key]
-    except KeyError:
-        pass
-    return default_value
-
-
-def get_course_id(event, from_url=False):
-    """Gets course_id from event's data."""
-
-    # Get the event data:
-    event_context = get_key_value_from_event(event, 'context')
-    if event_context is None:
-        # Assume it's old, and not worth logging...
-        return ''
-
-    # Get the course_id from the data, and validate.
-    course_id = opaque_key_util.normalize_course_id(get_key_value_from_event(event_context, 'course_id', ''))
-    if course_id:
-        if opaque_key_util.is_valid_course_id(course_id):
-            return course_id
-        else:
-            return ''  # we'll filter out empty course since string is expected
-
-    # Try to get the course_id from the URLs in `event_type` (for implicit
-    # server events) and `page` (for browser events).
-    if from_url:
-        source = get_key_value_from_event(event, 'event_source')
-
-        if source == 'server':
-            url = get_key_value_from_event(event, 'event_type', '')
-        elif source == 'browser':
-            url = get_key_value_from_event(event, 'page', '')
-        else:
-            url = ''
-
-        course_key = opaque_key_util.get_course_key_from_url(url)
-        if course_key:
-            return unicode(course_key)
-
-    return ''
 
 class UserActivityTask(OverwriteOutputMixin, WarehouseMixin, EventLogSelectionMixin, MultiOutputMapReduceJobTask):
     """
@@ -226,20 +154,10 @@ class UserActivityTaskSpark(EventLogSelectionMixinSpark, WarehouseMixin, SparkJo
     def output(self):
         return get_target_from_url(self.output_root)
 
-    # @property
-    # def files(self):
-    #     """
-    #     List of files to be placed in the working directory of each executor
-    #     """
-    #     files = super(UserActivityTaskSpark, self).files
-    #     if files is None:
-    #         files = []
-    #     files.append('/var/lib/analytics-tasks/automation/repo/edx/analytics/tasks/common/spark.py')
-    #     return files
-
     def spark_job(self):
         # adding file to spark context
-        self._spark_context.addPyFile(os.path.realpath(__file__))
+        self._load_external_dependency_on_cluster()     # all internal imports should be imported after this
+        from tasks.common.spark import get_course_id, get_event_predicate_labels
         from pyspark.sql.functions import udf, struct, split, explode
         from pyspark.sql.types import ArrayType, StringType
         df = self.get_event_log_dataframe(self._spark)
