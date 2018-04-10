1cbb4c653bdb9ddf4eb892a81022b9f6d1ab0618
Abdul Mannan <raoabdulmannan@gmail.com>
2018-04-10 14:51:10 +0500

    add delay while provisioning for hadoop services

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -44,6 +44,14 @@ docker exec -i edx.devstack.analytics_pipeline bash -c '/edx/app/hadoop/hive/bin
 
 # materialize hadoop directory structure
 echo -e "${GREEN}Initializing Hadoop directory structure...${NC}"
+
+until curl http://namenode:50070/jmx?qry=Hadoop:service=NameNode,name=NameNodeStatus|grep -q 'active'; do
+  printf "Waiting for namenode!"
+  sleep 5
+done
+sleep 30 # for datanode & other services to activate
+echo -e "${GREEN}Namenode is ready!${NC}"
+
 docker exec -i -u hadoop edx.devstack.analytics_pipeline bash -c 'sudo /edx/app/hadoop/hadoop/bin/hdfs dfs -chown -R hadoop:hadoop hdfs://namenode:8020/; hdfs dfs -mkdir -p hdfs://namenode:8020/edx-analytics-pipeline/{warehouse,marker,manifest,packages} hdfs://namenode:8020/{spark-warehouse,data} hdfs://namenode:8020/tmp/spark-events;hdfs dfs -copyFromLocal -f /edx/app/hadoop/lib/edx-analytics-hadoop-util.jar hdfs://namenode:8020/edx-analytics-pipeline/packages/;'
 
 echo -e "${GREEN}Provisioning complete!${NC}"
