41dd933c4e6ef06e718d33669dfcb68c872968a6
Hassan Javeed <hassan@edx.org>
2019-03-19 16:25:21 +0500

    test

--- edx/analytics/tasks/warehouse/financial/paypal.py
+++ edx/analytics/tasks/warehouse/financial/paypal.py
@@ -700,31 +700,10 @@ class PaypalTransactionsIntervalTask(PaypalTaskMixin, WarehouseMixin, luigi.Wrap
 
     def __init__(self, *args, **kwargs):
         super(PaypalTransactionsIntervalTask, self).__init__(*args, **kwargs)
-        # Provide default for output_root at this level.
-        if self.output_root is None:
-            self.output_root = self.warehouse_path
 
-        path = url_path_join(self.output_root, 'payments')
-        path_targets = PathSetTask([path], include=['*paypal.tsv']).output()
-        paths = list(set([os.path.dirname(target.path) for target in path_targets]))
-        dates = [path.rsplit('/', 2)[-1] for path in paths]
-        latest_date = sorted(dates)[-1]
-
-        latest_completion_date = datetime.datetime.strptime(latest_date, "dt=%Y-%m-%d").date()
-        run_date = latest_completion_date + datetime.timedelta(days=1)
-
-        self.selection_interval = date_interval.Custom(self.interval_start, run_date)
-        self.run_interval = date_interval.Custom(run_date, self.interval_end)
+        self.run_interval = date_interval.Custom(self.interval_start, self.interval_end)
 
     def requires(self):
-
-        yield PathSelectionByDateIntervalTask(
-            source=[url_path_join(self.warehouse_path, 'payments')],
-            interval=self.selection_interval,
-            pattern=['.*dt=(?P<date>\\d{4}-\\d{2}-\\d{2})/paypal\\.tsv'],
-            expand_interval=datetime.timedelta(0),
-            date_pattern='%Y-%m-%d',
-        )
 
         for day in self.run_interval:
             yield PaypalTransactionsByDayTask(
