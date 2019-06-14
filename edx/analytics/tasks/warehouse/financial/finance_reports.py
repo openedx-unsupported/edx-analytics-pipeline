384eb84d96a54bb27e07313ef4ecae50d75d5347
bmedx <bmesick@edx.org>
2019-06-14 13:28:19 -0400

    Add Affiliate Window pulls to finance reporting task

--- edx/analytics/tasks/warehouse/financial/finance_reports.py
+++ edx/analytics/tasks/warehouse/financial/finance_reports.py
@@ -6,6 +6,7 @@ from edx.analytics.tasks.common.vertica_load import VerticaCopyTaskMixin
 from edx.analytics.tasks.warehouse.financial.ed_services_financial_report import (
     LoadInternalReportingEdServicesReportToWarehouse
 )
+from edx.analytics.tasks.warehouse.financial.fees import LoadFeesToWarehouse
 from edx.analytics.tasks.warehouse.financial.reconcile import (
     LoadInternalReportingFullOrderTransactionsToWarehouse, LoadInternalReportingFullOttoOrdersToWarehouse,
     LoadInternalReportingFullShoppingcartOrdersToWarehouse, LoadInternalReportingOrderTransactionsToWarehouse,
