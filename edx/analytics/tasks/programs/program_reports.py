28460814194e0b9f43d5c8027b300f28f37b70f1
Zach Hancock <zhancock@edx.org>
2019-10-16 16:12:49 -0400

    cleanup/refactoring

--- edx/analytics/tasks/programs/program_reports.py
+++ edx/analytics/tasks/programs/program_reports.py
@@ -1,6 +1,5 @@
 import csv
 import datetime
-from itertools import islice
 import logging
 
 import luigi
@@ -12,7 +11,7 @@ from edx.analytics.tasks.util.url import ExternalURL, get_target_from_url, url_p
 
 log = logging.getLogger(__name__)
 
-class BuildProgramReportsTask(OverwriteOutputMixin, MultiOutputMapReduceJobTask):
+class BaseProgramReportsTask(OverwriteOutputMixin, MultiOutputMapReduceJobTask):
     """ Generates CSV reports on program enrollment """
 
     credentials = luigi.Parameter(
@@ -21,6 +20,9 @@ class BuildProgramReportsTask(OverwriteOutputMixin, MultiOutputMapReduceJobTask)
     )
     output_root = luigi.Parameter(
         description='URL pointing to the location reports should be stored',
+    )
+    report_name = luigi.Parameter(
+        description='Name of report file(s) to output'
     )
     date = luigi.Parameter(
         default=datetime.datetime.utcnow().date(),
@@ -57,7 +59,7 @@ class BuildProgramReportsTask(OverwriteOutputMixin, MultiOutputMapReduceJobTask)
     )
 
     def __init__(self, *args, **kwargs):
-        super(BuildProgramReportsTask, self).__init__(*args, **kwargs)
+        super(BaseProgramReportsTask, self).__init__(*args, **kwargs)
         self.columns = self.get_column_names()
 
     def requires(self):
@@ -72,42 +74,28 @@ class BuildProgramReportsTask(OverwriteOutputMixin, MultiOutputMapReduceJobTask)
             overwrite=False,
         )
 
-    def get_columns(self):
-        # TODO: any way to make this callable on __init__()?
-        table_schema = get_vertica_table_schema(
-            self.credentials,
-            self.schema_name,
-            self.table_name,
-        )
- 
-        column_list = []
-        for field_name, vertica_field_type, _ in table_schema:
-            column_list.append(field_name)
-        return column_list
-
     def get_column_names(self):
-        return [
-            'User ID',
-            'Program Title',
-            'Program ID',
-            'Program UUID',
-            'Course ID',
-            'Track',
-            'Created',
-        ]
+        """
+        List names of columns as they should appear in the CSV.
+
+        This must match the order they are stored in the exported warehouse table
+        """
+        return []  # should be implemented by child 
 
     def run(self):
         """
         Clear out output if overwrite requested.
         """
         self.remove_output_on_overwrite()
-
-        super(BuildProgramReportsTask, self).run()
+        super(BaseProgramReportsTask, self).run()
 
     def mapper(self, line):
-        """ Group input by program"""
-        program_id = line.split(',')[1]
-        yield program_id, line
+        """
+        Group input by program
+        """
+        # TODO: we should make first column the key (uuid)
+        program_uuid = line.split(',')[3]
+        yield program_uuid, line
 
     def multi_output_reducer(self, key, values, output_file):
         writer = csv.DictWriter(output_file, self.columns)
@@ -125,5 +113,31 @@ class BuildProgramReportsTask(OverwriteOutputMixin, MultiOutputMapReduceJobTask)
             writer.writerow(row_dict)
 
     def output_path_for_key(self, key):
-        filename = u'{}.csv'.format(self.date)
+        filename = u'{}__{}.csv'.format(self.report_name, self.date)
         return url_path_join(self.output_root, key, filename)
+
+
+class BuildLearnerProgramReportTask(BaseProgramReportsTask):
+    
+    table_name = luigi.Parameter(
+        default='learner_enrollments',
+    )
+    report_name = luigi.Parameter(
+        default='learner_report'
+    )
+   
+    def get_column_names(self):
+        """
+        List names of columns as they should appear in the CSV.
+
+        This must match the order they are stored in the exported warehouse table
+        """
+        return [
+            'User ID',
+            'Program ID',
+            'Program Title',
+            'Program UUID',
+            'Course ID',
+            'Track',
+            'Created',
+        ]
