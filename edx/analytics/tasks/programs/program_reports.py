a54802522369fbb484d159e793e6e69d3d8f882a
Zachary Hancock <zhancock@edx.org>
2019-10-24 09:59:46 -0400

    Learner Report by Program (#768)
    program learner report output

--- edx/analytics/tasks/programs/program_reports.py
+++ edx/analytics/tasks/programs/program_reports.py
@@ -1,16 +1,18 @@
+import csv
 import datetime
-from itertools import islice
 import logging
 
 import luigi
 
+from edx.analytics.tasks.common.mapreduce import MultiOutputMapReduceJobTask
 from edx.analytics.tasks.common.vertica_export import ExportVerticaTableToS3Task, get_vertica_table_schema
 from edx.analytics.tasks.util.overwrite import OverwriteOutputMixin
 from edx.analytics.tasks.util.url import ExternalURL, get_target_from_url, url_path_join
 
 log = logging.getLogger(__name__)
 
-class BuildProgramReportsTask(OverwriteOutputMixin, luigi.Task):
+
+class BaseProgramReportsTask(OverwriteOutputMixin, MultiOutputMapReduceJobTask):
     """ Generates CSV reports on program enrollment """
 
     credentials = luigi.Parameter(
@@ -19,6 +21,9 @@ class BuildProgramReportsTask(OverwriteOutputMixin, luigi.Task):
     )
     output_root = luigi.Parameter(
         description='URL pointing to the location reports should be stored',
+    )
+    report_name = luigi.Parameter(
+        description='Name of report file(s) to output'
     )
     date = luigi.Parameter(
         default=datetime.datetime.utcnow().date(),
@@ -41,7 +46,7 @@ class BuildProgramReportsTask(OverwriteOutputMixin, luigi.Task):
         description='A string replacement value for any (null) values encountered by Sqoop when exporting from Vertica.',
     )
     sqoop_fields_terminated_by = luigi.Parameter(
-        default=',',
+        default='\t',
         description='The field delimiter used by Sqoop.',
     )
     sqoop_delimiter_replacement = luigi.Parameter(
@@ -53,9 +58,16 @@ class BuildProgramReportsTask(OverwriteOutputMixin, luigi.Task):
         default=True,
         description='Whether or not to overwrite existing outputs',
     )
+    overwrite_export = luigi.BoolParameter(
+        default=False,
+        description='Whether or not to overwrite existing database export'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(BaseProgramReportsTask, self).__init__(*args, **kwargs)
+        self.columns = self.get_column_names()
 
     def requires(self):
-
         return ExportVerticaTableToS3Task(
             vertica_schema_name=self.schema_name,
             table_name=self.table_name,
@@ -64,38 +76,84 @@ class BuildProgramReportsTask(OverwriteOutputMixin, luigi.Task):
             sqoop_null_string=self.sqoop_null_string,
             sqoop_fields_terminated_by=self.sqoop_fields_terminated_by,
             sqoop_delimiter_replacement=self.sqoop_delimiter_replacement,
-            overwrite=False,
+            overwrite=(self.overwrite_export and self.overwrite),
         )
 
-    def print_result_head(self):
-        """Temp debug function to print head of file to console until we can access s3"""
-        with self.output().open('r') as result_file:
-            lines = result_file.read().splitlines()
-            log.info('--CSV CONTENT--')
-            log.info(''.join(lines[:100]))
+    def get_column_names(self):
+        """
+        List names of columns as they should appear in the CSV.
+
+        This must match the order they are stored in the exported warehouse table
+        """
+        return []  # should be implemented by child
 
     def run(self):
+        """
+        Clear out output if overwrite requested.
+        """
+        self.remove_output_on_overwrite()
+        super(BaseProgramReportsTask, self).run()
 
-        table_schema = get_vertica_table_schema(
-            self.credentials,
-            self.schema_name,
-            self.table_name,
-        )
- 
-        column_list = []
-        for field_name, vertica_field_type, _ in table_schema:
-            column_list.append(field_name)
+    def mapper(self, line):
+        """
+        Group input by authoring institution and program
+        """
+        (org, program_title, program_uuid, content) = line.split('\t', 3)
+        yield (org, program_uuid), line
 
-        with self.input().open('r') as input_file:
-            lines = input_file.read().splitlines()
+    def multi_output_reducer(self, key, values, output_file):
+        """
+        Map export values to report output fields and write to csv.  Drops any extra columns
+        """
+        writer = csv.DictWriter(output_file, self.columns)
+        writer.writeheader()
 
-        with self.output().open('w') as output_file:
-            header = ','.join(column_list)
-            output_file.write(header + '\n')
-            for line in lines:
-                output_file.write(line + '\n')
+        for content in values:
+            fields = content.split('\t')
+            row = {field_key: field_value for field_key, field_value in zip(self.columns, fields)}
+            writer.writerow(row)
 
-        self.print_result_head()
+    def output_path_for_key(self, key):
+        org_key, program_uuid = key
+        filename = u'{}__{}.csv'.format(self.report_name, self.date)
+        return url_path_join(self.output_root, org_key, program_uuid, filename)
 
-    def output(self):
-        return get_target_from_url(url_path_join(self.output_root, '{}.csv'.format(self.date)))
+
+class BuildLearnerProgramReportTask(BaseProgramReportsTask):
+
+    table_name = luigi.Parameter(
+        default='learner_enrollments',
+    )
+    report_name = luigi.Parameter(
+        default='learner_report'
+    )
+
+    @staticmethod
+    def get_column_names():
+        """
+        List names of columns as they should appear in the CSV.
+
+        This must match the order they are stored in the exported warehouse table
+        """
+        return [
+            'Authoring Institution',
+            'Program Title',
+            'Program UUID',
+            'Program Type',
+            'User ID',
+            'Username',
+            'Name',
+            'User Key',
+            'Course Title',
+            'Course Run Key',
+            'External Course Key',
+            'Track',
+            'Grade',
+            'Letter Grade',
+            'Date First Enrolled',
+            'Date Last Unenrolled',
+            'Currently Enrolled',
+            'Date First Upgraded to Verified',
+            'Completed',
+            'Date Completed'
+        ]
