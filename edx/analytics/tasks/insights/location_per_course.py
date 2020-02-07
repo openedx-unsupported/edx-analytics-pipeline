ab0686b8f758e5c5dcc5f9cbcc88c37f3a9ccb1a
Troy Sankey <sankeytms@gmail.com>
2020-02-07 00:22:22 -0500

    debugging

--- edx/analytics/tasks/insights/location_per_course.py
+++ edx/analytics/tasks/insights/location_per_course.py
@@ -292,6 +292,10 @@ class LastCountryOfUser(LastCountryOfUserDownstreamMixin, GeolocationMixin, MapR
                 expand_interval=datetime.timedelta(0),
                 date_pattern='%Y-%m-%d',
             )
+            log.debug("DEBUG path_selection_interval: {}".format(str(path_selection_interval)))
+            log.debug("DEBUG last_ip_of_user_root: {}".format(str(last_ip_of_user_root)))
+            log.debug("DEBUG path_selection_task: {}".format(str(path_selection_task)))
+            log.debug("DEBUG path_selection_task.output(): {}".format(str(path_selection_task.output())))
 
             requirements = {
                 'path_selection_task': path_selection_task,
@@ -675,6 +679,10 @@ class LastCityOfUser(LastCountryOfUserDownstreamMixin, GeolocationMixin, MapRedu
                 expand_interval=datetime.timedelta(0),
                 date_pattern='%Y-%m-%d',
             )
+            log.debug("DEBUG path_selection_interval: {}".format(str(path_selection_interval)))
+            log.debug("DEBUG last_ip_of_user_root: {}".format(str(last_ip_of_user_root)))
+            log.debug("DEBUG path_selection_task: {}".format(str(path_selection_task)))
+            log.debug("DEBUG path_selection_task.output(): {}".format(str(path_selection_task.output())))
 
             requirements = {
                 'path_selection_task': path_selection_task,
