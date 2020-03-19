a1a0e554d16dbb3e538ca360210f845991371356
irfanuddinahmad <irfan.ahmad@arbisoft.com>
2020-03-19 18:15:08 +0500

    Added task for creating engagement report

--- edx/analytics/tasks/enterprise/enterprise_engagement_report.py
+++ edx/analytics/tasks/enterprise/enterprise_engagement_report.py
@@ -1,40 +1,20 @@
-"""Compute metrics related to enterprise engagement in courses"""
-
-import datetime
-import logging
-import os
-
+"""Generates an enterprise learner engagement report"""
 import luigi
-import luigi.task
 
 from edx.analytics.tasks.common.mysql_load import MysqlInsertTask
-from edx.analytics.tasks.common.pathutil import PathSetTask
+from edx.analytics.tasks.common.mapreduce import MapReduceJobTaskMixin
 from edx.analytics.tasks.enterprise.enterprise_database_imports import (
-    ImportBenefitTask, ImportConditionalOfferTask, ImportDataSharingConsentTask,
-    ImportEnterpriseCourseEnrollmentUserTask, ImportEnterpriseCustomerTask, ImportEnterpriseCustomerUserTask,
-    ImportUserSocialAuthTask, ImportVoucherTask
+    ImportEnterpriseCustomerTask, ImportEnterpriseCustomerUserTask
 )
-from edx.analytics.tasks.insights.database_imports import (
-    ImportAuthUserProfileTask, ImportAuthUserTask, ImportCourseEntitlementTask, ImportCurrentOrderDiscountState,
-    ImportCurrentOrderLineState, ImportCurrentOrderState, ImportEcommerceUser, ImportPersistentCourseGradeTask,
-    ImportProductCatalog, ImportProductCatalogAttributes, ImportProductCatalogAttributeValues,
-    ImportStudentCourseEnrollmentTask
-)
-from edx.analytics.tasks.insights.enrollments import (
-    CourseEnrollmentSummaryPartitionTask, OverwriteHiveAndMysqlDownstreamMixin
-)
-from edx.analytics.tasks.insights.module_engagement import ModuleEngagementPartitionTask
-from edx.analytics.tasks.util.decorators import workflow_entry_point
-from edx.analytics.tasks.util.hive import BareHiveTableTask, HivePartitionTask, OverwriteAwareHiveQueryDataTask
-from edx.analytics.tasks.util.record import (
-    BooleanField, DateField, DateTimeField, FloatField, IntegerField, Record, StringField
-)
-from edx.analytics.tasks.util.url import ExternalURL, url_path_join
-from edx.analytics.tasks.warehouse.load_internal_reporting_course_catalog import (
-    CoursePartitionTask, LoadInternalReportingCourseCatalogMixin
-)
+from edx.analytics.tasks.insights.database_imports import DatabaseImportMixin
+from edx.analytics.tasks.insights.module_engagement import ModuleEngagementWorkflowTask
 
-log = logging.getLogger(__name__)
+from edx.analytics.tasks.util.hive import HivePartition, HiveTableFromQueryTask
+from edx.analytics.tasks.util.record import (
+    DateTimeField, IntegerField, Record, StringField
+)
+from edx.analytics.tasks.warehouse.load_internal_reporting_course_structure import AllCourseBlockRecordsTask
+from edx.analytics.tasks.warehouse.load_internal_reporting_user import AggregateInternalReportingUserTableHive
 
 
 class EnterpriseEngagementRecord(Record):
@@ -66,56 +46,57 @@ class EnterpriseEngagementRecord(Record):
     enterprise_name = StringField(length=255, nullable=False, description='')
 
 
-class EnterpriseEngagementHiveTableTask(BareHiveTableTask):
+class BuildEnterpriseEngagementReportTask(DatabaseImportMixin, MapReduceJobTaskMixin, HiveTableFromQueryTask):
     """
-    Creates the metadata for the enterprise_engagement hive table
+    Builds the enterprise learner engagement report.
     """
-    @property  # pragma: no cover
-    def partition_by(self):
-        return 'dt'
+
+    def requires(self):
+        for requirement in super(BuildEnterpriseEngagementReportTask, self).requires():
+            yield requirement
+
+        yield (
+            AggregateInternalReportingUserTableHive(
+                n_reduce_tasks=self.n_reduce_tasks,
+                date=self.date,
+                warehouse_path=self.warehouse_path,
+                overwrite=self.overwrite
+            ),
+            ImportEnterpriseCustomerTask(
+                import_date=self.import_date
+            ),
+            ImportEnterpriseCustomerUserTask(
+                import_date=self.import_date
+            ),
+            ModuleEngagementWorkflowTask(
+                date=self.date,
+                n_reduce_tasks=self.n_reduce_tasks
+            ),
+            AllCourseBlockRecordsTask(
+                date=self.date,
+                warehouse_path=self.warehouse_path,
+                overwrite=self.overwrite
+            )
+        )
 
     @property
-    def table(self):  # pragma: no cover
-        return 'enterprise_engagement'
+    def table(self):
+        return 'enterprise_engagement_report'
 
     @property
     def columns(self):
         return EnterpriseEngagementRecord.get_hive_schema()
 
-
-class EnterpriseEngagementHivePartitionTask(HivePartitionTask):
-    """
-    Generates the enterprise_engagement hive partition.
-    """
-    date = luigi.DateParameter()
-
     @property
-    def hive_table_task(self):  # pragma: no cover
-        return EnterpriseEngagementHiveTableTask(
-            warehouse_path=self.warehouse_path,
-            overwrite=self.overwrite
-        )
-
-    @property
-    def partition_value(self):  # pragma: no cover
-        """ Use a dynamic partition value based on the date parameter. """
-        return self.date.isoformat()  # pylint: disable=no-member
-
-
-class EnterpriseEngagementDataTask(
-    OverwriteHiveAndMysqlDownstreamMixin,
-    LoadInternalReportingCourseCatalogMixin,
-    OverwriteAwareHiveQueryDataTask
-):
-    """
-    Executes a hive query to gather enterprise engagement data and store it in the enterprise_engagement hive table.
-    """
+    def partition(self):
+        return HivePartition('dt', self.import_date.isoformat())  # pylint: disable=no-member
 
     @property
     def insert_query(self):
-        """The query builder that controls the structure and fields inserted into the new table."""
         return """
             SELECT
+                    ec.enterprise_customer_uuid,
+                    ec.enterprise_customer_name,
                     me.course_id,
                     struct.section_block_id,
                     sect.display_name as section_display_name,
@@ -133,13 +114,13 @@ class EnterpriseEngagementDataTask(
                     struct.lms_web_url
             FROM
                     /*Get information on the component name.*/
-                    production.course_structure as struct
+                    course_block_records as struct
             LEFT JOIN
-                    production.course_structure as sect
+                    course_block_records as sect
             ON
                     struct.section_block_id = sect.block_id
             LEFT JOIN
-                    production.course_structure as subsect
+                    course_block_records as subsect
                     
             ON
                     struct.subsection_block_id = subsect.block_id 
@@ -148,13 +129,13 @@ class EnterpriseEngagementDataTask(
                       not include all data. Production version built
                       on top of read replica should check that lifetime
                       data is piping through.*/
-                    insights.module_engagement as me
+                    module_engagement as me
             ON
                     struct.block_id = me.entity_id
             LEFT JOIN
-                    production.d_user as pdu
+                    internal_reporting_user as pdu
             ON
-                    me.username = pdu.user_username
+                    me.username = pdu.username
             LEFT JOIN (
                     /*Create rank "section_index" that says "this is the 1st
                       section in the course, the second section in the course,
@@ -165,7 +146,7 @@ class EnterpriseEngagementDataTask(
                             section_block_id,
                             MAX(order_index) as order_index
                     from 
-                            production.course_structure
+                            course_block_records
                     where
                             /*Filter out higher level blocks 
                             so indexing is not thrown off.*/
@@ -196,7 +177,7 @@ class EnterpriseEngagementDataTask(
                             subsection_block_id,
                             MAX(order_index) as order_index
                     FROM 
-                            production.course_structure
+                            course_block_records
                     WHERE
                             /*Filter out higher level blocks 
                             so indexing is not thrown off.*/
@@ -217,84 +198,42 @@ class EnterpriseEngagementDataTask(
             ON
                     struct.subsection_block_id = subsection_.subsection_block_id
             JOIN
-                    enterprise.base_enterprise_user as eu
+                    enterprise_enterprisecustomeruser as eu
             ON
-                    pdu.user_id = eu.lms_user_id
+                    pdu.user_id = eu.user_id
             JOIN
-                    enterprise.base_enterprise_customer as ec
+                    enterprise_enterprisecustomer as ec
             ON
-                    eu.enterprise_customer_uuid = ec.enterprise_customer_uuid
+                    eu.enterprise_customer_id = ec.uuid
             WHERE
-                    /* Naive filter to have only Pearson records. Because there is future
-                    potential of other customers have access this, good to build in such a
-                    way that this can be generalized to any enterprise customer name.*/
-                    ec.enterprise_customer_name = 'Pearson'
-            AND
                     /* Per edX legal review, throw out all discussion data 
                     from delivery.*/
                     me.entity_type != 'discussion'
             ORDER BY
                     me.course_id,
-                    me.username,
-                    struct.order_index{code}
+                    me.username
+            ;
         """
 
-    @property
-    def hive_partition_task(self):  # pragma: no cover
-        """The task that creates the partition used by this job."""
-        return EnterpriseEngagementHivePartitionTask(
-            date=self.date,
-            warehouse_path=self.warehouse_path,
-            overwrite=self.overwrite,
-        )
 
-    def requires(self):  # pragma: no cover
-        for requirement in super(EnterpriseEngagementDataTask, self).requires():
-            yield requirement
-
-        # the process that generates the source table used by this query
-        yield (
-            ImportAuthUserTask(),
-            ImportAuthUserProfileTask(),
-            ImportEnterpriseCustomerTask(),
-            ImportEnterpriseCustomerUserTask(),
-            ModuleEngagementPartitionTask(
-                n_reduce_tasks=self.n_reduce_tasks,
-                warehouse_path=self.warehouse_path,
-                overwrite=self.overwrite_hive,
-            ),
-            AllCourseBlockRecordsTask(
-                date=self.date,
-                warehouse_path=self.warehouse_path,
-                overwrite=self.overwrite_hive,
-            )
-        )
-
-
-class EnterpriseEngagementMysqlTask(
-    OverwriteHiveAndMysqlDownstreamMixin,
-    LoadInternalReportingCourseCatalogMixin,
-    MysqlInsertTask
-):
+class EnterpriseEngagementMysqlTask(MysqlInsertTask):
     """
     All engagements of enterprise users in courses associated with their enterprise.
     """
+    import_date = luigi.DateParameter()
 
     @property
     def table(self):  # pragma: no cover
-        return 'enterprise_engagement'
+        return 'enterprise_learner_engagement'
 
     @property
     def insert_source_task(self):  # pragma: no cover
-        return EnterpriseEngagementDataTask(
-            warehouse_path=self.warehouse_path,
-            overwrite_hive=self.overwrite_hive,
-            overwrite_mysql=self.overwrite_mysql,
-            overwrite=self.overwrite,
-            date=self.date,
-            n_reduce_tasks=self.n_reduce_tasks,
+        return (
+            BuildEnterpriseEngagementReportTask(
+                import_date=self.import_date,
+                n_reduce_tasks=self.n_reduce_tasks
+            )
         )
-
     @property
     def columns(self):
         return EnterpriseEngagementRecord.get_sql_schema()
@@ -302,24 +241,16 @@ class EnterpriseEngagementMysqlTask(
     @property
     def indexes(self):
         return [
-            ('enterprise_id',),
+            ('enterprise_id', 'course_id'),
         ]
 
 
-@workflow_entry_point
-class ImportEnterpriseEngagementIntoMysql(
-    OverwriteHiveAndMysqlDownstreamMixin,
-    LoadInternalReportingCourseCatalogMixin,
-    luigi.WrapperTask
-):
+class ImportEnterpriseEngagementsIntoMysql(luigi.WrapperTask):
     """Import enterprise engagement data into MySQL."""
 
     def requires(self):
         kwargs = {
             'warehouse_path': self.warehouse_path,
-            'overwrite_hive': self.overwrite_hive,
-            'overwrite_mysql': self.overwrite_mysql,
-            'overwrite': self.overwrite_hive,
             'date': self.date,
             'n_reduce_tasks' = self.n_reduce_tasks,
         }
